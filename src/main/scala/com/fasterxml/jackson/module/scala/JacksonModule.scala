1bd982d99a1ff6e0bfd2486259b3987583580339
Christopher Currie <christopher@currie.com>
2011-10-04 00:04:40 -0700

    Streamlining of JacksonModule

--- src/main/scala/com/fasterxml/jackson/module/scala/JacksonModule.scala
+++ src/main/scala/com/fasterxml/jackson/module/scala/JacksonModule.scala
@@ -5,28 +5,25 @@ import org.codehaus.jackson.map.Module.SetupContext
 import org.codehaus.jackson.map.{Deserializers, Serializers, Module}
 import org.codehaus.jackson.map.`type`.TypeModifier
 import org.codehaus.jackson.map.ser.BeanSerializerModifier
+
 trait JacksonModule extends Module {
 
-  private val serializers = Seq.newBuilder[Serializers]
-  private val deserializers = Seq.newBuilder[Deserializers]
-  private val typeModifiers = Seq.newBuilder[TypeModifier]
-  private val beanSerModifiers = Seq.newBuilder[BeanSerializerModifier]
+  private val initializers = Seq.newBuilder[SetupContext => Unit]
 
   def getModuleName = "JacksonModule"
 
   // TODO: Keep in sync with POM
   val version = new Version(1,9,0,"SNAPSHOT")
 
-  def setupModule(context: SetupContext)
-  {
-    serializers.result().foreach(context.addSerializers(_))
-    deserializers.result().foreach(context.addDeserializers(_))
-    typeModifiers.result().foreach(context.addTypeModifier(_))
-    beanSerModifiers.result().foreach(context.addBeanSerializerModifier(_))
+  def setupModule(context: SetupContext) { initializers result() foreach (_ apply context) }
+
+  protected def +=(ser: Serializers): this.type = { initializers += (_ addSerializers ser); this }
+  protected def +=(deser: Deserializers): this.type = { initializers += (_ addDeserializers deser); this }
+  protected def +=(typeMod: TypeModifier): this.type = { initializers += (_ addTypeModifier typeMod); this }
+
+  protected def +=(beanSerMod: BeanSerializerModifier): this.type = {
+    initializers += (_ addBeanSerializerModifier beanSerMod)
+    this
   }
 
-  protected def +=(ser: Serializers) = serializers += ser
-  protected def +=(deser: Deserializers) = deserializers += deser
-  protected def +=(typeMod: TypeModifier) = typeModifiers += typeMod
-  protected def +=(beanSerMod: BeanSerializerModifier) = beanSerModifiers += beanSerMod
 }
