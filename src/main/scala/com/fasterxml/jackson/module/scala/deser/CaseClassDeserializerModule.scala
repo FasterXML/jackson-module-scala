4b40c7f842b16e2a63b47e9a39a2e7cb66356429
Christopher Currie <ccurrie@impresys.com>
2011-12-01 19:12:44 -0800

    Filter CaseClassAnnotationIntrospector attempt to only work on case classes.

--- src/main/scala/com/fasterxml/jackson/module/scala/deser/CaseClassDeserializerModule.scala
+++ src/main/scala/com/fasterxml/jackson/module/scala/deser/CaseClassDeserializerModule.scala
@@ -6,29 +6,47 @@ import org.codehaus.jackson.map.introspect.{AnnotatedField, AnnotatedConstructor
 import org.scalastuff.scalabeans.{DeserializablePropertyDescriptor, ConstructorParameter}
 
 private object CaseClassAnnotationIntrospector extends NopAnnotationIntrospector {
-  override def findDeserializablePropertyName(af: AnnotatedField): String = {
-    val cls = af.getDeclaringClass
-    val descriptor = descriptorOf(cls)
-
-    descriptor.properties.find {
-      case dp: DeserializablePropertyDescriptor => af.getName.equals(dp.name)
-      case _ => false
-    } map (_.name) getOrElse null
+  private def maybeIsCaseClass(cls: Class[_]): Boolean = {
+    if (!classOf[Product].isAssignableFrom(cls)) false
+    else if (cls.getName.startsWith("scala.Tuple")) false
+    else true
   }
 
-  override def findPropertyNameForParam(param: AnnotatedParameter): String = param.getOwner match {
-    case _: AnnotatedConstructor => findConstructorParamName(param)
-    case _ => null
+  override def findDeserializablePropertyName(af: AnnotatedField): String = {
+    val cls = af.getDeclaringClass
+    if (!maybeIsCaseClass(cls)) null
+    else {
+      val descriptor = descriptorOf(cls)
+
+      descriptor.properties.find {
+        case dp: DeserializablePropertyDescriptor => af.getName.equals(dp.name)
+        case _ => false
+      } map (_.name) getOrElse null
+    }
+  }
+
+  override def findPropertyNameForParam(param: AnnotatedParameter): String = {
+    val cls = param.getDeclaringClass
+    if (!maybeIsCaseClass(cls)) null
+    else {
+      param.getOwner match {
+        case _: AnnotatedConstructor => findConstructorParamName(param)
+        case _ => null
+      }
+    }
   }
 
   private def findConstructorParamName(param: AnnotatedParameter): String = {
     val cls = param.getDeclaringClass
-    val descriptor = descriptorOf(cls)
+    if (!maybeIsCaseClass(cls)) null
+    else {
+      val descriptor = descriptorOf(cls)
 
-    descriptor.properties.find {
-      case cp: ConstructorParameter => cp.index == param.getIndex
-      case _ => false
-    }.map(_.name) getOrElse null
+      descriptor.properties.find {
+        case cp: ConstructorParameter => cp.index == param.getIndex
+        case _ => false
+      }.map(_.name) getOrElse null
+    }
   }
 
 }
