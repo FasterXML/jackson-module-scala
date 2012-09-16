93c85568fa7378ff2edf93d5c09fc64bc2974d27
svenwiegand <forum@sven-wiegand.de>
2012-09-16 17:23:58 +0200

    Implemented changes for scala 2.10.0-M7

--- src/main/scala/com/fasterxml/jackson/module/scala/deser/CaseClassDeserializerModule.scala
+++ src/main/scala/com/fasterxml/jackson/module/scala/deser/CaseClassDeserializerModule.scala
@@ -1,63 +1,41 @@
 package com.fasterxml.jackson.module.scala.deser
 
+
 import com.fasterxml.jackson.module.scala.JacksonModule
-import com.fasterxml.jackson.module.scala.util.ScalaBeansUtil
 
-import com.fasterxml.jackson.databind.introspect.{AnnotatedField, AnnotatedConstructor, AnnotatedParameter, NopAnnotationIntrospector};
+import com.fasterxml.jackson.databind.introspect.{AnnotatedField, AnnotatedConstructor, AnnotatedParameter, NopAnnotationIntrospector}
 
-import org.scalastuff.scalabeans.{DeserializablePropertyDescriptor, ConstructorParameter}
+import com.fasterxml.jackson.module.scala.reflect.{MethodSignature, BeanMirror}
 
 private object CaseClassAnnotationIntrospector extends NopAnnotationIntrospector {
-  lazy val PRODUCT = classOf[Product]
-  lazy val OPTION = classOf[Option[_]]
-  lazy val LIST = classOf[List[_]]
-
-  private def maybeIsCaseClass(cls: Class[_]): Boolean = {
-    if (!PRODUCT.isAssignableFrom(cls)) false
-    else if (OPTION.isAssignableFrom(cls)) false
-    else if (LIST.isAssignableFrom(cls)) false
-    else if (cls.getName.startsWith("scala.Tuple")) false
-    else true
-  }
-
   override def findDeserializationName(af: AnnotatedField): String = {
-    val cls = af.getDeclaringClass
-    if (!maybeIsCaseClass(cls)) null
-    else {
-      val properties = ScalaBeansUtil.propertiesOf(cls)
-
-      properties.find {
-        case dp: DeserializablePropertyDescriptor => af.getName.equals(dp.name)
-        case _ => false
-      } map (_.name) getOrElse null
+    try {
+      if (BeanMirror(af.getDeclaringClass).hasSetter(af.getName, af.getRawType)) "" else null
+    } catch {
+      case _: IllegalArgumentException => null
     }
   }
 
   override def findDeserializationName(param: AnnotatedParameter): String = {
-    val cls = param.getDeclaringClass
-    if (!maybeIsCaseClass(cls)) null
-    else {
-      param.getOwner match {
-        case _: AnnotatedConstructor => findConstructorParamName(param)
-        case _ => null
+    param.getOwner match {
+      case constructor: AnnotatedConstructor => findConstructorParamName(constructor, param)
+      case _ => null
+    }
+  }
+
+  private def findConstructorParamName(constructor: AnnotatedConstructor, param: AnnotatedParameter): String = {
+    try {
+      BeanMirror(constructor.getDeclaringClass).
+        getConstructorParameterName(MethodSignature(constructor), param.getIndex) match {
+        case Some(name) => name
+        case None => null
       }
+    } catch {
+      case _: IllegalArgumentException => null
     }
   }
-
-  private def findConstructorParamName(param: AnnotatedParameter): String = {
-    val cls = param.getDeclaringClass
-    if (!maybeIsCaseClass(cls)) null
-    else {
-      val properties = ScalaBeansUtil.propertiesOf(cls)
-
-      properties.find {
-        case cp: ConstructorParameter => cp.index == param.getIndex
-        case _ => false
-      }.map(_.name) getOrElse null
-    }
-  }
-
 }
+
 
 trait CaseClassDeserializerModule extends JacksonModule {
   this += { _.appendAnnotationIntrospector(CaseClassAnnotationIntrospector) }
