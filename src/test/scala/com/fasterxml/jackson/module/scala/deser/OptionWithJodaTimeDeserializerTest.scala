59b80eb598aa8523a03e2cca5ed3bb55433fe6e2
Christopher Currie <christopher@currie.com>
2012-09-18 21:31:59 -0700

    Test to try to repro #33

---
+++ src/test/scala/com/fasterxml/jackson/module/scala/deser/OptionWithJodaTimeDeserializerTest.scala
@@ -0,0 +1,27 @@
+package com.fasterxml.jackson.module.scala.deser
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.FlatSpec
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.datatype.joda.JodaModule
+
+private case class OptionalInt(x: Option[Int])
+
+@RunWith(classOf[JUnitRunner])
+class OptionWithJodaTimeDeserializerTest extends DeserializerTest with FlatSpec with ShouldMatchers {
+
+  def module = DefaultScalaModule
+
+  "DefaultScalaModule" should "deserialize a case class with Option without JodaModule" in {
+    deserialize[OptionalInt](stringValue) should be (OptionalInt(Some(123)))
+  }
+
+  it should "deserialize a case clas with Option with JodaModule" in {
+    mapper.registerModule(new JodaModule)
+    deserialize[OptionalInt](stringValue) should be (OptionalInt(Some(123)))
+  }
+
+  val stringValue = """{"x":123}"""
+}
