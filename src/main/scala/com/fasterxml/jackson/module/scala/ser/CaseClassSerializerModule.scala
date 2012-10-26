a6849b1028a9e1db88f9bd85b069b246e2692012
Christopher Currie <christopher@currie.com>
2012-10-25 21:42:24 -0700

    Case class inclusion. Fix #32, #45

--- src/main/scala/com/fasterxml/jackson/module/scala/ser/CaseClassSerializerModule.scala
+++ src/main/scala/com/fasterxml/jackson/module/scala/ser/CaseClassSerializerModule.scala
@@ -6,9 +6,10 @@ import java.{util => ju}
 
 import org.scalastuff.scalabeans.ConstructorParameter
 
-import com.fasterxml.jackson.databind.{PropertyName, BeanDescription, PropertyNamingStrategy, SerializationConfig}
-;
-import com.fasterxml.jackson.databind.ser.{BeanPropertyWriter, BeanSerializerModifier};
+import com.fasterxml.jackson.annotation.{JsonInclude, JsonIgnore}
+import com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL
+import com.fasterxml.jackson.databind.{PropertyName, BeanDescription, SerializationConfig}
+import com.fasterxml.jackson.databind.ser.{BeanPropertyWriter, BeanSerializerModifier}
 import com.fasterxml.jackson.databind.introspect.AnnotatedMethod
 import com.fasterxml.jackson.databind.util.SimpleBeanPropertyDefinition
 import com.fasterxml.jackson.module.scala.JacksonModule
@@ -22,30 +23,36 @@ private object CaseClassBeanSerializerModifier extends BeanSerializerModifier {
                                 beanDesc: BeanDescription,
                                 beanProperties: ju.List[BeanPropertyWriter]): ju.List[BeanPropertyWriter] = {
     val jacksonIntrospector = config.getAnnotationIntrospector
+    val defaultInclusion = beanDesc.findSerializationInclusion(config.getSerializationInclusion)
+
     val list = for {
       cls <- Option(beanDesc.getBeanClass).toSeq if (PRODUCT.isAssignableFrom(cls))
-      prop <- ScalaBeansUtil.propertiesOf(cls)
+      prop <- ScalaBeansUtil.propertiesOf(cls) if (prop.findAnnotation[JsonIgnore].map(!_.value).getOrElse(true))
       // Not completely happy with this test. I'd rather check the PropertyDescription
       // to see if it's a field or a method, but ScalaBeans doesn't expose that as yet.
       // I'm not sure if it truly matters as Scala generates method accessors for fields.
       // This is also realy inefficient, as we're doing a find on each iteration of the loop.
       method <- Option(beanDesc.findMethod(prop.name, Array()))
-    } yield prop match {
-      case cp: ConstructorParameter =>
-        val param = beanDesc.getConstructors.get(0).getParameter(cp.index)
-        asWriter(config, beanDesc, method, Option(jacksonIntrospector.findNameForDeserialization(param)))
-      case _ => asWriter(config, beanDesc, method)
+    } yield {
+      // Check for the JsonInclude annotation
+      val suppressNulls = prop.findAnnotation[JsonInclude].getOrElse(defaultInclusion) == NON_NULL
+      prop match {
+        case cp: ConstructorParameter =>
+          val param = beanDesc.getConstructors.get(0).getParameter(cp.index)
+          asWriter(config, beanDesc, method, suppressNulls, Option(jacksonIntrospector.findNameForDeserialization(param)))
+        case _ => asWriter(config, beanDesc, method, suppressNulls)
+      }
     }
 
     if (list.isEmpty) beanProperties else new ju.ArrayList[BeanPropertyWriter](list.toList.asJava)
   }
 
-  private def asWriter(config: SerializationConfig, beanDesc: BeanDescription, member: AnnotatedMethod, primaryName: Option[PropertyName] = None) = {
+  private def asWriter(config: SerializationConfig, beanDesc: BeanDescription, member: AnnotatedMethod, suppressNulls: Boolean, primaryName: Option[PropertyName] = None) = {
     val javaType = config.getTypeFactory.constructType(member.getGenericType)
     val defaultName = NameTransformer.decode(member.getName)
     val name = maybeTranslateName(config, member, primaryName.map(_.toString).getOrElse(defaultName))
     val propDef = new SimpleBeanPropertyDefinition(member, name)
-    new BeanPropertyWriter(propDef, member, null, javaType, null, null, null, false, null)
+    new BeanPropertyWriter(propDef, member, null, javaType, null, null, null, suppressNulls, null)
   }
 
   private def maybeTranslateName(config: SerializationConfig, member: AnnotatedMethod, name: String) = {
