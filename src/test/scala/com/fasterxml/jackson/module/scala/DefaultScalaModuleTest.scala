8aa63ab7720649ac8d291294be516b85cba76a16
Christopher Currie <christopher@currie.com>
2013-06-24 23:24:46 -0700

    Test to ensure Version is sane

---
+++ src/test/scala/com/fasterxml/jackson/module/scala/DefaultScalaModuleTest.scala
@@ -0,0 +1,18 @@
+package com.fasterxml.jackson.module.scala
+
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.FlatSpec
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class DefaultScalaModuleTest extends FlatSpec with ShouldMatchers {
+
+  "DefaultScalaModule" should "have a sensible version" in {
+    val version = DefaultScalaModule.version
+    version.getMajorVersion should be >= 2
+    version.getArtifactId should be ("jackson-module-scala")
+    version.getGroupId should be ("com.fasterxml.jackson.module")
+  }
+
+}
