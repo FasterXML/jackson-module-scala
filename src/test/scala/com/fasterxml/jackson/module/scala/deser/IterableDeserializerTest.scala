4a8c841a927844dcc5520546a5a91f32682668ba
Christopher Currie <christopher@currie.com>
2013-12-20 10:33:31 -0800

    Iterable support

---
+++ src/test/scala/com/fasterxml/jackson/module/scala/deser/IterableDeserializerTest.scala
@@ -0,0 +1,20 @@
+package com.fasterxml.jackson.module.scala.deser
+
+import org.scalatest.matchers.ShouldMatchers
+
+class IterableDeserializerTest extends DeserializationFixture with ShouldMatchers {
+
+  // Testing values
+  val listJson =  "[1,2,3,4,5,6]"
+  val listScala = 1 to 6
+
+  behavior of "ObjectMapper"
+
+  it should "deserialize a list to an Iterable" in { f =>
+
+    val result = f.readValue[Iterable[Int]](listJson)
+    result should equal (listScala)
+
+  }
+
+}
