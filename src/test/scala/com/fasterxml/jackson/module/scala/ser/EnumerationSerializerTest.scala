96bfd66c9ac2d04e6908e446ada949686ed98ef5
Christopher Currie <ccurrie@impresys.com>
2013-12-24 10:41:22 -0800

    Test for custom Enumeration names (#118)

--- src/test/scala/com/fasterxml/jackson/module/scala/ser/EnumerationSerializerTest.scala
+++ src/test/scala/com/fasterxml/jackson/module/scala/ser/EnumerationSerializerTest.scala
@@ -1,17 +1,33 @@
-package com.fasterxml.jackson.module.scala.ser
+package com.fasterxml.jackson
+package module.scala
+package ser
 
 import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.FlatSpec
 import org.scalatest.junit.JUnitRunner
 import org.junit.runner.RunWith
-import com.fasterxml.jackson.module.scala.{DefaultScalaModule, JsonScalaEnumeration, Weekday}
-import com.fasterxml.jackson.core.`type`.TypeReference
+
+import core.`type`.TypeReference
 
 class WeekdayType extends TypeReference[Weekday.type]
 case class AnnotationHolder(@JsonScalaEnumeration(classOf[WeekdayType]) weekday: Weekday.Weekday)
 
+object EnumerationSerializerTest
+{
+  object OptionType extends Enumeration {
+    val STRING = Value("string")
+    val NUMBER = Value("number")
+    val BOOLEAN = Value("boolean")
+  }
+
+  class OptionTypeReference extends TypeReference[OptionType.type]
+  case class OptionTypeHolder(@JsonScalaEnumeration(classOf[OptionTypeReference]) optionType: OptionType.Value)
+}
+
 @RunWith(classOf[JUnitRunner])
 class EnumerationSerializerTest extends SerializerTest with FlatSpec with ShouldMatchers {
+
+  import EnumerationSerializerTest._
 
   lazy val module = DefaultScalaModule
 
@@ -26,5 +42,9 @@ class EnumerationSerializerTest extends SerializerTest with FlatSpec with Should
 		val day = Weekday.Fri
 		serialize(day) should be ("""{"enumClass":"com.fasterxml.jackson.module.scala.Weekday","value":"Fri"}""")
 	}
+
+  it should "serialize an annotated Enumeration with custom values" in {
+    serialize(OptionTypeHolder(OptionType.STRING)) should be ("""{"optionType":"string"}""")
+  }
 
 }
