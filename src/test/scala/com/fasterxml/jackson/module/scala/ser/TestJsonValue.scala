cdd71877488de496af3d5b27888440e46ca0619a
Christopher Currie <christopher@currie.com>
2014-02-11 22:02:07 -0800

    Test and fix for #124

---
+++ src/test/scala/com/fasterxml/jackson/module/scala/ser/TestJsonValue.scala
@@ -0,0 +1,26 @@
+package com.fasterxml.jackson.module.scala.ser
+
+import com.fasterxml.jackson.databind.Module
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.annotation.JsonValue
+import org.scalatest.matchers.ShouldMatchers
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FlatSpec
+import scala.annotation.target.getter
+
+object TestJsonValue
+{
+  case class ValueClass(@(JsonValue @getter) value: String)
+}
+
+@RunWith(classOf[JUnitRunner])
+class TestJsonValue extends SerializerTest with FlatSpec with ShouldMatchers {
+  import TestJsonValue._
+
+  override def module: Module = DefaultScalaModule
+
+  "DefaultScalaModule" should "support @JsonValue" in {
+    serialize(ValueClass("Foo")) should equal (""""Foo"""")
+  }
+}
