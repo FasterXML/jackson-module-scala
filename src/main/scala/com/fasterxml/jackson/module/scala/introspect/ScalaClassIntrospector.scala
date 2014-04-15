f714299b9241243b9e27c221ec6bd6562dc425f3
Christopher Currie <christopher@currie.com>
2014-04-14 21:48:23 -0700

    Expression cleanup

--- src/main/scala/com/fasterxml/jackson/module/scala/introspect/ScalaClassIntrospector.scala
+++ src/main/scala/com/fasterxml/jackson/module/scala/introspect/ScalaClassIntrospector.scala
@@ -9,7 +9,7 @@ import com.fasterxml.jackson.module.scala.util.Implicits._
 object ScalaClassIntrospector extends BasicClassIntrospector {
 
   private def isScalaPackage(pkg: Option[Package]): Boolean =
-    pkg flatMap { _.getName.split("\\.").headOption } map { _ == "scala" } getOrElse false
+    pkg flatMap { _.getName.split("\\.").headOption } exists { _ == "scala" }
 
   private def isMaybeScalaBeanType(cls: Class[_]): Boolean =
     cls.hasSignature && !isScalaPackage(Option(cls.getPackage))
