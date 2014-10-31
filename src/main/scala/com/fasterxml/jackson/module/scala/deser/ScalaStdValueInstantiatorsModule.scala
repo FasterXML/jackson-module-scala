abd85ac1d455a4a11e8ab6ff40e3d4eb6ca7de0d
Christopher Currie <codemonkey@usermind.com>
2014-10-31 15:57:39 -0700

    Fixes #167

--- src/main/scala/com/fasterxml/jackson/module/scala/deser/ScalaStdValueInstantiatorsModule.scala
+++ src/main/scala/com/fasterxml/jackson/module/scala/deser/ScalaStdValueInstantiatorsModule.scala
@@ -1,34 +1,49 @@
-package com.fasterxml.jackson.module.scala.deser
+package com.fasterxml.jackson
+package module.scala
+package deser
 
-import com.fasterxml.jackson.module.scala.JacksonModule
-import com.fasterxml.jackson.databind.deser.{ValueInstantiator, ValueInstantiators, Deserializers}
-import com.fasterxml.jackson.databind._
-import com.fasterxml.jackson.databind.deser.std.StdValueInstantiator
+import com.fasterxml.jackson.core.JsonToken.{START_ARRAY, VALUE_NUMBER_FLOAT, VALUE_NUMBER_INT, VALUE_STRING}
+import com.fasterxml.jackson.core.{JsonParser, JsonToken}
+import com.fasterxml.jackson.databind.{JsonDeserializer, BeanDescription, DeserializationConfig, JavaType, DeserializationContext}
+import com.fasterxml.jackson.databind.DeserializationFeature.UNWRAP_SINGLE_VALUE_ARRAYS
+import com.fasterxml.jackson.databind.deser.Deserializers
+import com.fasterxml.jackson.databind.deser.std.StdScalarDeserializer
 
-class BigDecimalInstantiator(config: DeserializationConfig) extends StdValueInstantiator(config, classOf[BigDecimal]) {
+object BigDecimalDeserializer extends StdScalarDeserializer[BigDecimal](classOf[BigDecimal]) {
+  override def deserialize(jp: JsonParser, ctxt: DeserializationContext): BigDecimal = {
+    val t = jp.getCurrentToken
+    t match {
+      case VALUE_NUMBER_INT | VALUE_NUMBER_FLOAT | VALUE_STRING =>
+        val text = jp.getText.trim
+        if (text.length == 0)
+          null
+        else
+          BigDecimal(text)
 
-  override def canCreateFromInt = true
-  override def createFromInt(ctx: DeserializationContext, value: Int) = BigDecimal(value)
-  override def canCreateFromLong = true
-  override def createFromLong(ctx: DeserializationContext, value: Long) = BigDecimal(value)
-  override def canCreateFromDouble = true
-  override def createFromDouble(ctx: DeserializationContext, value: Double) = BigDecimal(value)
-  override def canCreateFromString = true
-  override def createFromString(ctx: DeserializationContext, value: String) = BigDecimal(value)
-}
+      case START_ARRAY if ctxt.isEnabled(UNWRAP_SINGLE_VALUE_ARRAYS) =>
+        jp.nextToken()
+        val value = deserialize(jp, ctxt)
+        if (jp.nextToken() != JsonToken.END_ARRAY) {
+          throw ctxt.wrongTokenException(jp, JsonToken.END_ARRAY, "Attempted to unwrap single value array for single 'BigDecimal' value but there was more than a single value in the array")
+        }
+        value
 
-object ScalaStdValueInstatiators extends ValueInstantiators {
-  val BIG_DECIMAL = classOf[BigDecimal]
-
-  override def findValueInstantiator(config: DeserializationConfig, beanDesc: BeanDescription, defaultInstantiator: ValueInstantiator): ValueInstantiator = {
-    if (BIG_DECIMAL.isAssignableFrom(beanDesc.getBeanClass))
-      new BigDecimalInstantiator(config)
-    else
-      defaultInstantiator
+      case _ =>
+        throw ctxt.mappingException(_valueClass, t)
+    }
   }
-
 }
 
 trait ScalaStdValueInstantiatorsModule extends JacksonModule {
-  this += (_.addValueInstantiators(ScalaStdValueInstatiators))
+  this += (_.addDeserializers(new Deserializers.Base {
+    override def findBeanDeserializer(`type`: JavaType,
+                                      config: DeserializationConfig,
+                                      beanDesc: BeanDescription): JsonDeserializer[_] = {
+      if (`type`.getRawClass == BigDecimalDeserializer.handledType) {
+        return BigDecimalDeserializer
+      }
+
+      null
+    }
+  }))
 }
