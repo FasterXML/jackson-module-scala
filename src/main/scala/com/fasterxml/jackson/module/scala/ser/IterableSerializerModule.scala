650ddffd0e083c8d6a6c4764811a6136465b2767
Christopher Currie <christopher@currie.com>
2014-12-28 20:30:03 -0800

    Fixes #172

--- src/main/scala/com/fasterxml/jackson/module/scala/ser/IterableSerializerModule.scala
+++ src/main/scala/com/fasterxml/jackson/module/scala/ser/IterableSerializerModule.scala
@@ -2,24 +2,33 @@ package com.fasterxml.jackson.module.scala.ser
 
 import collection.JavaConverters._
 
-import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonGenerator
 
 import com.fasterxml.jackson.databind._
-import com.fasterxml.jackson.databind.`type`.CollectionLikeType;
-import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
-import com.fasterxml.jackson.databind.ser.Serializers;
-import com.fasterxml.jackson.databind.ser.std.{AsArraySerializerBase, CollectionSerializer};
+import com.fasterxml.jackson.databind.`type`.CollectionLikeType
+import com.fasterxml.jackson.databind.jsontype.TypeSerializer
+import com.fasterxml.jackson.databind.ser.Serializers
+import com.fasterxml.jackson.databind.ser.std.{AsArraySerializerBase, CollectionSerializer}
 
 import com.fasterxml.jackson.module.scala.modifiers.IterableTypeModifierModule
 import collection.Iterable
 
 private class IterableSerializer(seqType: Class[_],
                                  elemType: JavaType,
+                                 anyRefType: JavaType,
                                  staticTyping: Boolean,
                                  vts: Option[TypeSerializer],
                                  property: BeanProperty,
                                  valueSerializer: Option[JsonSerializer[AnyRef]])
-  extends AsArraySerializerBase[collection.Iterable[Any]](seqType, elemType, staticTyping, vts.orNull, property, valueSerializer.orNull) {
+  // CollectionSerializer *needs* an elementType, but AsArraySerializerBase *forces*
+  // static typing if the element type is final. This makes sense to Java, but Scala
+  // corrupts the Java type system in the case of "ValueTypes"; the signature of the
+  // collection is marked as the underlying type, but the storage actually holds the
+  // value type, causing casts that Jackson does to fail.
+  //
+  // The workaround is to let Jackson know that it can't rely on the element type
+  // by telling it the element type is AnyRef.
+  extends AsArraySerializerBase[collection.Iterable[Any]](seqType, anyRefType, staticTyping, vts.orNull, property, valueSerializer.orNull) {
 
   def hasSingleElement(p1: Iterable[Any]) = p1.take(2).size == 1
 
@@ -34,7 +43,7 @@ private class IterableSerializer(seqType: Class[_],
     withResolved(property, newVts, valueSerializer.orNull)
 
   override def withResolved(newProperty: BeanProperty, newVts: TypeSerializer, elementSerializer: JsonSerializer[_]) =
-    new IterableSerializer(seqType, elemType, staticTyping, Option(newVts), newProperty, Option(elementSerializer.asInstanceOf[JsonSerializer[AnyRef]]))
+    new IterableSerializer(seqType, elemType, anyRefType, staticTyping, Option(newVts), newProperty, Option(elementSerializer.asInstanceOf[JsonSerializer[AnyRef]]))
 
   override def isEmpty(value: collection.Iterable[Any]): Boolean = value.isEmpty
 }
@@ -49,7 +58,7 @@ private object IterableSerializerResolver extends Serializers.Base {
     val rawClass = collectionType.getRawClass
     if (!classOf[collection.Iterable[Any]].isAssignableFrom(rawClass)) null else
     if (classOf[collection.Map[Any,Any]].isAssignableFrom(rawClass)) null else
-    new IterableSerializer(rawClass, collectionType.containedType(0), false, Option(elementTypeSerializer), null, Option(elementSerializer))
+    new IterableSerializer(rawClass, collectionType.containedType(0), config.constructType(classOf[AnyRef]), false, Option(elementTypeSerializer), null, Option(elementSerializer))
   }
 
 }
