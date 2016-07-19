25ced9bc9cd51e4ee348ed6a6278b5f7cbba72fd
Jakob Odersky <jakob@odersky.com>
2016-07-18 18:01:18 -0700

    Update build for Scala 2.12.0-M5
    * Upgrade 2.12 version to latest milestone, M5
    * Upgrade Scalatest version to work with latest Scala version

--- src/test/scala/com/fasterxml/jackson/module/scala/deser/OptionWithJodaTimeDeserializerTest.scala
+++ src/test/scala/com/fasterxml/jackson/module/scala/deser/OptionWithJodaTimeDeserializerTest.scala
@@ -2,7 +2,7 @@ package com.fasterxml.jackson.module.scala.deser
 
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
-import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.Matchers
 import org.scalatest.FlatSpec
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.fasterxml.jackson.datatype.joda.JodaModule
