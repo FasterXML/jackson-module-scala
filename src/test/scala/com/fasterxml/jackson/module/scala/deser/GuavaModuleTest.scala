5a3ae5292e1afd63d1005003edb9b9486e866b3b
PJ Fanning <pj@kontainers.co.uk>
2019-09-26 08:32:40 +0100

    use scalatest in guava test

--- src/test/scala/com/fasterxml/jackson/module/scala/deser/GuavaModuleTest.scala
+++ src/test/scala/com/fasterxml/jackson/module/scala/deser/GuavaModuleTest.scala
@@ -1,51 +1,50 @@
 package com.fasterxml.jackson.module.scala.deser
 
-import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper, ObjectReader}
+import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper}
 import com.fasterxml.jackson.datatype.guava.GuavaModule
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.google.common.collect.Multimap
-import org.junit.Assert.assertNotNull
-import org.junit.Test
+import org.junit.runner.RunWith
+import org.scalatest.{FlatSpec, Matchers}
+import org.scalatestplus.junit.JUnitRunner
 
 class PojoWithMultiMap(val headers: Multimap[String, String])
 
-class GuavaModuleTest {
-  @Test
-  def testScalaIsSecond(): Unit = {
+@RunWith(classOf[JUnitRunner])
+class GuavaModuleTest extends FlatSpec with Matchers {
+  "Scala module" should "work with GuavaModule (Scala registered second)" in {
     val objectMapper = new ObjectMapper
     objectMapper.registerModule(new GuavaModule)
     objectMapper.registerModule(new DefaultScalaModule)
-    val objectReader = objectMapper.reader.asInstanceOf[ObjectReader].without(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+    val objectReader = objectMapper.reader.without(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+    val source = "{\"headers\":{\"key1\": [\"value1\"] }}"
+
+    val pojoWithMultiMap : PojoWithMultiMap = objectReader.readValue(objectReader.treeAsTokens(objectReader.readTree(source)),
+      objectMapper.getTypeFactory.constructType(classOf[PojoWithMultiMap]))
+    Option(pojoWithMultiMap) shouldBe defined
+  }
+
+  "Scala module" should "work with GuavaModule (Scala registered first)" in {
+    val objectMapper = new ObjectMapper
+    objectMapper.registerModule(new DefaultScalaModule)
+    objectMapper.registerModule(new GuavaModule)
+    val objectReader = objectMapper.reader.without(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
     val source: String = "{\"headers\":{\"key1\": [\"value1\"] }}"
 
     val pojoWithMultiMap : PojoWithMultiMap = objectReader.readValue(objectReader.treeAsTokens(objectReader.readTree(source)),
       objectMapper.getTypeFactory.constructType(classOf[PojoWithMultiMap]))
-    assertNotNull(pojoWithMultiMap)
+    Option(pojoWithMultiMap) shouldBe defined
   }
 
-  @Test
-  def testScalaIsFirst(): Unit = {
+  "Scala module" should "work with Guava MultiMap" in {
     val objectMapper = new ObjectMapper
     objectMapper.registerModule(new DefaultScalaModule)
     objectMapper.registerModule(new GuavaModule)
-    val objectReader = objectMapper.reader.asInstanceOf[ObjectReader].without(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-    val source: String = "{\"headers\":{\"key1\": [\"value1\"] }}"
-
-    val pojoWithMultiMap : PojoWithMultiMap = objectReader.readValue(objectReader.treeAsTokens(objectReader.readTree(source)),
-      objectMapper.getTypeFactory.constructType(classOf[PojoWithMultiMap]))
-    assertNotNull(pojoWithMultiMap)
-  }
-
-  @Test
-  def testNotPropertyBased(): Unit = {
-    val objectMapper = new ObjectMapper
-    objectMapper.registerModule(new DefaultScalaModule)
-    objectMapper.registerModule(new GuavaModule)
-    val objectReader = objectMapper.reader.asInstanceOf[ObjectReader].without(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-    val source: String = "{\"key1\": [\"value1\"] }"
+    val objectReader = objectMapper.reader.without(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+    val source = "{\"key1\": [\"value1\"] }"
 
     val multiMap : com.google.common.collect.Multimap[String,String] = objectReader.readValue(objectReader.treeAsTokens(objectReader.readTree(source)),
       objectMapper.getTypeFactory.constructMapLikeType(classOf[com.google.common.collect.Multimap[String,String]],classOf[String],classOf[String]))
-    assertNotNull(multiMap)
+    Option(multiMap) shouldBe defined
   }
 }
