bb236f77f627e231941d360a45ef06486933751f
PJ Fanning <pfanning@descartes.com>
2020-07-13 09:44:45 +0200

    add SwaggerTest

--- src/test/scala/com/fasterxml/jackson/module/scala/ser/SwaggerTest.scala
+++ src/test/scala/com/fasterxml/jackson/module/scala/ser/SwaggerTest.scala
@@ -2,7 +2,7 @@ package com.fasterxml.jackson.module.scala.ser
 
 import java.util
 
-import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.json.JsonMapper
 import com.fasterxml.jackson.module.scala.{BaseSpec, DefaultScalaModule}
 import io.swagger.models.ModelImpl
 import io.swagger.models.properties.{IntegerProperty, Property}
@@ -13,8 +13,8 @@ import org.scalatestplus.junit.JUnitRunner
 class SwaggerTest extends BaseSpec {
   //https://github.com/FasterXML/jackson-module-scala/issues/454
   "An ObjectMapper" should "serialize a Swagger Model" in {
-    val mapper = new ObjectMapper
-    mapper.registerModule(new DefaultScalaModule)
+    val builder = JsonMapper.builder().addModule(new DefaultScalaModule)
+    val mapper = builder.build()
     val model = new ModelImpl
     val property = new IntegerProperty
     val map = new util.HashMap[String, Property]()
