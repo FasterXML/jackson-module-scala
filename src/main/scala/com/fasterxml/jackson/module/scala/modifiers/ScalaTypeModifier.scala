5a269c7165c65054037a68a27e9cef0a7c446b23
PJ Fanning <pfanning@descartes.com>
2020-08-09 13:06:30 +0200

    revert some changes that caused test issues in scala 2.13 build

--- src/main/scala/com/fasterxml/jackson/module/scala/modifiers/ScalaTypeModifier.scala
+++ src/main/scala/com/fasterxml/jackson/module/scala/modifiers/ScalaTypeModifier.scala
@@ -6,28 +6,23 @@ import com.fasterxml.jackson.databind.JavaType
 import com.fasterxml.jackson.databind.`type`._
 import com.fasterxml.jackson.module.scala.JacksonModule
 
+import scala.collection._
+
 class ScalaTypeModifier extends TypeModifier {
-
-  private val optionClass = classOf[Option[_]]
-  private val eitherClass = classOf[Either[_, _]]
-  private val mapClass = classOf[Map[_, _]]
-  private val iterableOnceClass = classOf[IterableOnce[_]]
-
   override def modifyType(javaType: JavaType,
                           jdkType: Type,
                           context: TypeBindings,
                           typeFactory: TypeFactory): JavaType = {
 
-    if (javaType.isTypeOrSubTypeOf(optionClass)) {
-      javaType match {
-        case rt: ReferenceType => rt
-        case _ => ReferenceType.upgradeFrom(javaType, javaType.containedTypeOrUnknown(0))
-      }
-    } else if (javaType.isTypeOrSubTypeOf(mapClass)) {
+
+    if (javaType.isTypeOrSubTypeOf(classOf[Option[_]])) {
+      if (javaType.isInstanceOf[ReferenceType]) return javaType
+      ReferenceType.upgradeFrom(javaType, javaType.containedTypeOrUnknown(0))
+    } else if (javaType.isTypeOrSubTypeOf(classOf[Map[_, _]])) {
       MapLikeType.upgradeFrom(javaType, javaType.containedTypeOrUnknown(0), javaType.containedTypeOrUnknown(1))
-    } else if (javaType.isTypeOrSubTypeOf(iterableOnceClass)) {
+    } else if (javaType.isTypeOrSubTypeOf(classOf[IterableOnce[_]])) {
       CollectionLikeType.upgradeFrom(javaType, javaType.containedTypeOrUnknown(0))
-    } else if (javaType.isTypeOrSubTypeOf(eitherClass)) {
+    } else if (javaType.isTypeOrSubTypeOf(classOf[Either[_,_]])) {
       // I'm not sure this is the right choice, but it's what the original module does
       ReferenceType.upgradeFrom(javaType, javaType)
     } else {
