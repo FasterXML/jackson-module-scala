c66d012e8dc1184bb39a705d3ccfce1501d6e8db
PJ Fanning <pfanning@descartes.com>
2020-08-09 13:11:44 +0200

    redo refactor

--- src/main/scala/com/fasterxml/jackson/module/scala/modifiers/ScalaTypeModifier.scala
+++ src/main/scala/com/fasterxml/jackson/module/scala/modifiers/ScalaTypeModifier.scala
@@ -6,23 +6,27 @@ import com.fasterxml.jackson.databind.JavaType
 import com.fasterxml.jackson.databind.`type`._
 import com.fasterxml.jackson.module.scala.JacksonModule
 
-import scala.collection._
-
 class ScalaTypeModifier extends TypeModifier {
+
+  private val optionClass = classOf[Option[_]]
+  private val eitherClass = classOf[Either[_, _]]
+  private val mapClass = classOf[scala.collection.Map[_, _]]
+  private val iterableOnceClass = classOf[scala.collection.IterableOnce[_]]
+
   override def modifyType(javaType: JavaType,
                           jdkType: Type,
                           context: TypeBindings,
                           typeFactory: TypeFactory): JavaType = {
 
 
-    if (javaType.isTypeOrSubTypeOf(classOf[Option[_]])) {
+    if (javaType.isTypeOrSubTypeOf(optionClass)) {
       if (javaType.isInstanceOf[ReferenceType]) return javaType
       ReferenceType.upgradeFrom(javaType, javaType.containedTypeOrUnknown(0))
-    } else if (javaType.isTypeOrSubTypeOf(classOf[Map[_, _]])) {
+    } else if (javaType.isTypeOrSubTypeOf(mapClass)) {
       MapLikeType.upgradeFrom(javaType, javaType.containedTypeOrUnknown(0), javaType.containedTypeOrUnknown(1))
-    } else if (javaType.isTypeOrSubTypeOf(classOf[IterableOnce[_]])) {
+    } else if (javaType.isTypeOrSubTypeOf(iterableOnceClass)) {
       CollectionLikeType.upgradeFrom(javaType, javaType.containedTypeOrUnknown(0))
-    } else if (javaType.isTypeOrSubTypeOf(classOf[Either[_,_]])) {
+    } else if (javaType.isTypeOrSubTypeOf(eitherClass)) {
       // I'm not sure this is the right choice, but it's what the original module does
       ReferenceType.upgradeFrom(javaType, javaType)
     } else {
