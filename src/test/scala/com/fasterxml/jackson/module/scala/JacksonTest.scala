08936baca8e8c6ae451eef08e24376a3792f96dc
PJ Fanning <pfanning@descartes.com>
2020-08-26 18:54:44 +0200

    update to use JsonMapper.builder in tests

--- src/test/scala/com/fasterxml/jackson/module/scala/JacksonTest.scala
+++ src/test/scala/com/fasterxml/jackson/module/scala/JacksonTest.scala
@@ -1,13 +1,14 @@
 package com.fasterxml.jackson.module.scala
 
 import com.fasterxml.jackson.databind.{Module, ObjectMapper}
+import com.fasterxml.jackson.databind.json.JsonMapper
 
 abstract class JacksonTest extends BaseSpec {
   def module: Module
 
-  def newMapper: ObjectMapper = {
-    val result = new ObjectMapper
-    result.registerModule(module)
-    result
+  def newBuilder: JsonMapper.Builder = {
+    JsonMapper.builder().addModule(module)
   }
+
+  def newMapper: ObjectMapper = newBuilder.build()
 }
