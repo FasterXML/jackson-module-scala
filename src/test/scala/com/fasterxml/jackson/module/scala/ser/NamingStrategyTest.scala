20103422151111e5a8a78739ed7b5dfd7bcbdd26
PJ Fanning <pjfanning@users.noreply.github.com>
2020-12-06 17:55:36 +0000

    Update NamingStrategyTest.scala

--- src/test/scala/com/fasterxml/jackson/module/scala/ser/NamingStrategyTest.scala
+++ src/test/scala/com/fasterxml/jackson/module/scala/ser/NamingStrategyTest.scala
@@ -5,7 +5,6 @@ package ser
 import java.io.ByteArrayOutputStream
 
 import com.fasterxml.jackson.databind.{ObjectMapper, PropertyNamingStrategy}
-import com.google.common.base.Optional
 import org.junit.runner.RunWith
 import org.scalatest.flatspec.FixtureAnyFlatSpec
 import org.scalatest.Outcome
@@ -15,8 +14,7 @@ import org.scalatestplus.junit.JUnitRunner
 import scala.beans.BeanProperty
 
 class PojoWrittenInScala {
-  @BeanProperty var optFoo: Optional[String] = Optional.absent()
-  @BeanProperty var bar: Int = 0
+  @BeanProperty var fooBar: String = ""
 }
 
 @RunWith(classOf[JUnitRunner])
@@ -34,5 +32,9 @@ class NamingStrategyTest extends FixtureAnyFlatSpec with Matchers {
   "DefaultScalaModule" should "correctly handle naming strategies" in { mapper =>
     val bytes = new ByteArrayOutputStream()
     mapper.writeValue(bytes, new PojoWrittenInScala)
+    bytes.close()
+    bytes.toString should include("foo_bar")
+    val pojo = mapper.readValue(bytes.toByteArray, classOf[PojoWrittenInScala])
+    pojo.getFooBar() shouldEqual ""
   }
 }
