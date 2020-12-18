e880d880dd56ebd0da639129b38636b3115e145a
PJ Fanning <pjfanning@users.noreply.github.com>
2020-12-18 21:25:39 +0000

    start removal of FactorySorter

--- src/main/scala/com/fasterxml/jackson/module/scala/deser/UnsortedSetDeserializerModule.scala
+++ src/main/scala/com/fasterxml/jackson/module/scala/deser/UnsortedSetDeserializerModule.scala
@@ -2,7 +2,6 @@ package com.fasterxml.jackson.module.scala.deser
 
 import com.fasterxml.jackson.databind.JavaType
 import com.fasterxml.jackson.module.scala.modifiers.ScalaTypeModifierModule
-import com.fasterxml.jackson.module.scala.util.FactorySorter
 
 import scala.collection._
 
@@ -11,15 +10,15 @@ trait UnsortedSetDeserializerModule extends ScalaTypeModifierModule {
 
     override val CLASS_DOMAIN: Class[Collection[_]] = classOf[Set[_]]
 
-    override val factories: Iterable[(Class[_], Factory)] = new FactorySorter[Collection, IterableFactory]()
-      .add(Set)
-      .add(immutable.HashSet)
-      .add(immutable.ListSet)
-      .add(immutable.Set)
-      .add(mutable.HashSet)
-      .add(mutable.LinkedHashSet)
-      .add(mutable.Set)
-      .toList
+    override val factories: Iterable[(Class[_], Factory)] = sortFactories(Vector(
+      (classOf[Set[_]], Set),
+      (classOf[immutable.HashSet[_]], immutable.HashSet),
+      (classOf[immutable.ListSet[_]], immutable.ListSet),
+      (classOf[immutable.Set[_]], immutable.Set),
+      (classOf[mutable.HashSet[_]], mutable.HashSet),
+      (classOf[mutable.LinkedHashSet[_]], mutable.LinkedHashSet),
+      (classOf[mutable.Set[_]], mutable.Set)
+    ))
 
     override def builderFor[A](cf: Factory, javaType: JavaType): Builder[A] = cf.newBuilder[A]
   })
