1f20e2e3aa822ec2aae10cf68c1fecad70894d03
PJ Fanning <pjfanning@users.noreply.github.com>
2020-12-18 21:25:39 +0000

    remove FactorySorter

--- src/main/scala/com/fasterxml/jackson/module/scala/deser/SortedSetDeserializerModule.scala
+++ src/main/scala/com/fasterxml/jackson/module/scala/deser/SortedSetDeserializerModule.scala
@@ -3,7 +3,6 @@ package com.fasterxml.jackson.module.scala.deser
 import com.fasterxml.jackson.databind.{DeserializationConfig, JavaType}
 import com.fasterxml.jackson.module.scala.introspect.OrderingLocator
 import com.fasterxml.jackson.module.scala.modifiers.ScalaTypeModifierModule
-import com.fasterxml.jackson.module.scala.util.FactorySorter
 
 import scala.collection._
 
@@ -12,13 +11,13 @@ trait SortedSetDeserializerModule extends ScalaTypeModifierModule {
 
     override val CLASS_DOMAIN: Class[Collection[_]] = classOf[SortedSet[_]]
 
-    override val factories: Iterable[(Class[_], Factory)] = new FactorySorter[Collection, SortedIterableFactory]()
-      .add(SortedSet)
-      .add(immutable.TreeSet)
-      .add(immutable.SortedSet)
-      .add(mutable.TreeSet)
-      .add(mutable.SortedSet)
-      .toList
+    override val factories: Iterable[(Class[_], Factory)] = sortFactories(Vector(
+      (classOf[SortedSet[_]], SortedSet),
+      (classOf[immutable.TreeSet[_]], immutable.TreeSet),
+      (classOf[immutable.SortedSet[_]], immutable.SortedSet),
+      (classOf[mutable.TreeSet[_]], mutable.TreeSet),
+      (classOf[mutable.SortedSet[_]], mutable.SortedSet)
+    ))
 
     override def builderFor[A](cf: Factory, valueType: JavaType): Builder[A] =
       cf.newBuilder[A](OrderingLocator.locate(valueType).asInstanceOf[Ordering[A]])
