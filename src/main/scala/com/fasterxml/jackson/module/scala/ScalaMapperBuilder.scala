4c385bdda5fe9fa32aea84cdfa5f9ef3c131a59c
PJ Fanning <pjfanning@users.noreply.github.com>
2021-02-10 15:40:10 +0000

    Create ScalaMapperBuilder.scala

---
+++ src/main/scala/com/fasterxml/jackson/module/scala/ScalaMapperBuilder.scala
@@ -0,0 +1,27 @@
+package com.fasterxml.jackson.module.scala
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.cfg.MapperBuilder
+
+/**
+ * @tparam M
+ * @tparam B
+ * @since 2.13.0
+ */
+trait ScalaMapperBuilder[M <: ObjectMapper,B <: MapperBuilder[M,B]] {
+  self: MapperBuilder[M,B] =>
+
+  /**
+   * Method to use for adding mix-in annotations to use for augmenting
+   * specified class or interface. All annotations from
+   * <code>mixinSource</code> are taken to override annotations
+   * that <code>target</code> (or its supertypes) has.
+   *
+   * @tparam Target Class (or interface) whose annotations to effectively override
+   * @tparam MixinSource Class (or interface) whose annotations are to
+   *                     be "added" to target's annotations, overriding as necessary
+   */
+  final def addMixin[Target: Manifest, MixinSource: Manifest](): B = {
+    addMixIn(manifest[Target].runtimeClass, manifest[MixinSource].runtimeClass)
+  }
+}
