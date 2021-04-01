85e11761e58e21751fcdf83c776556ca331dd1dc
PJ Fanning <pjfanning@users.noreply.github.com>
2021-04-01 23:19:43 +0100

    Update NamingStrategyTest.scala

--- src/test/scala/com/fasterxml/jackson/module/scala/ser/NamingStrategyTest.scala
+++ src/test/scala/com/fasterxml/jackson/module/scala/ser/NamingStrategyTest.scala
@@ -31,6 +31,6 @@ class NamingStrategyTest extends FixtureAnyFlatSpec with Matchers {
     bytes.close()
     bytes.toString should include("foo_bar")
     val pojo = mapper.readValue(bytes.toByteArray, classOf[PojoWrittenInScala])
-    pojo.getFooBar() shouldEqual ""
+    pojo.fooBar shouldEqual ""
   }
 }
