067b119f696c28a2cbae1655cabea92b11f8b8e7
PJ Fanning <pjfanning@users.noreply.github.com>
2021-07-27 12:53:27 +0100

    move some tests that fail with Scala3

--- src/test/scala/com/fasterxml/jackson/module/scala/ser/AnyValSerializerTest.scala
+++ src/test/scala/com/fasterxml/jackson/module/scala/ser/AnyValSerializerTest.scala
@@ -1,48 +1,20 @@
 package com.fasterxml.jackson.module.scala.ser
 
-import com.fasterxml.jackson.annotation.JsonValue
 import com.fasterxml.jackson.module.scala.BaseFixture
 
-import scala.annotation.meta.getter
-
 object AnyValSerializerTest {
-
-  case class IntAnyVal(@(JsonValue @getter) underlying: Int) extends AnyVal
-  case class IntAnyValHolder(value: IntAnyVal)
-
-  case class LI(values: List[IntAnyVal])
-  case class IntIter(values: Iterator[IntAnyVal])
-
   case class DoubleAnyVal(underlying: Double) extends AnyVal
   case class DoubleAnyValHolder(value: DoubleAnyVal)
 
   case class BigIntAnyVal(underlying: BigInt) extends AnyVal
   case class BigIntAnyValHolder(value: BigIntAnyVal)
-
-  case class BD(@(JsonValue @getter) underlying: BigDecimal) extends AnyVal
-  case class BDH(value: BD)
-  case class LBD(values: List[BD])
 }
 
+//see AnyValScala2SerializerTest for cases that only work with Scala2
 class AnyValSerializerTest extends BaseFixture {
   import AnyValSerializerTest._
 
   behavior of "AnyVal"
-
-  it should "serialize an Int AnyVal" in { mapper =>
-    val value = IntAnyVal(42)
-    mapper.writeValueAsString(value) shouldBe """42"""
-    mapper.writeValueAsString(IntAnyValHolder(value)) shouldBe """{"value":42}"""
-  }
-
-  it should "serialize a list of Int AnyVal" in { mapper =>
-    mapper.writeValueAsString(LI(List(IntAnyVal(42), IntAnyVal(99)))) shouldBe """{"values":[42,99]}"""
-  }
-
-  it should "serialize a Iterator of Int AnyVal" in { mapper =>
-    val list = List(IntAnyVal(42), IntAnyVal(99))
-    mapper.writeValueAsString(IntIter(list.iterator)) shouldBe """{"values":[42,99]}"""
-  }
 
   it should "serialize an Double AnyVal" in { mapper =>
     val value = DoubleAnyVal(42)
@@ -56,13 +28,4 @@ class AnyValSerializerTest extends BaseFixture {
     mapper.writeValueAsString(BigIntAnyValHolder(value)) shouldBe """{"value":42}"""
   }
 
-  it should "serialize an BigDecimal AnyVal" in { mapper =>
-    val value = BD(42)
-    mapper.writeValueAsString(value) shouldBe "42"
-    mapper.writeValueAsString(BDH(value)) shouldBe """{"value":42}"""
-  }
-
-  it should "serialize a List of BigDecimal AnyVal" in { mapper =>
-    mapper.writeValueAsString(LBD(List(BD(42), BD(99)))) shouldBe """{"values":[42,99]}"""
-  }
 }
