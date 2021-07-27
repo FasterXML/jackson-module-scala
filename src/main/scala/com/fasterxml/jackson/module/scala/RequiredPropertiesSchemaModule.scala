463f3fd37ae8da052f698d5e71a5b38de9a79cad
PJ Fanning <pjfanning@users.noreply.github.com>
2021-07-27 12:10:45 +0100

    deprecate DefaultRequiredAnnotationIntrospector

--- src/main/scala/com/fasterxml/jackson/module/scala/RequiredPropertiesSchemaModule.scala
+++ src/main/scala/com/fasterxml/jackson/module/scala/RequiredPropertiesSchemaModule.scala
@@ -3,6 +3,7 @@ package com.fasterxml.jackson.module.scala
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.introspect.{AnnotatedMember, NopAnnotationIntrospector}
 
+@deprecated("will be removed in 3.0.0 release as jackson-module-jsonSchema is being discontinued", "2.13.0")
 object DefaultRequiredAnnotationIntrospector extends NopAnnotationIntrospector {
 
   private val OPTION = classOf[Option[_]]
@@ -16,6 +17,7 @@ object DefaultRequiredAnnotationIntrospector extends NopAnnotationIntrospector {
 
 }
 
+@deprecated("will be removed in 3.0.0 release as jackson-module-jsonSchema is being discontinued", "2.13.0")
 trait RequiredPropertiesSchemaModule extends JacksonModule {
   this += { _.insertAnnotationIntrospector(DefaultRequiredAnnotationIntrospector) }
 }
