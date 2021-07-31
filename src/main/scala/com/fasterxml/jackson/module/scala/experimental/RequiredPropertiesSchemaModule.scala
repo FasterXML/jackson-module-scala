09d9b44107fd3b067e190088d904082a8b4a44bb
PJ Fanning <pjfanning@users.noreply.github.com>
2021-07-31 19:18:41 +0100

    Update RequiredPropertiesSchemaModule.scala

--- src/main/scala/com/fasterxml/jackson/module/scala/experimental/RequiredPropertiesSchemaModule.scala
+++ src/main/scala/com/fasterxml/jackson/module/scala/experimental/RequiredPropertiesSchemaModule.scala
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.databind.introspect.{AnnotatedMember, NopAnnotation
 /**
  * @deprecated use {@link com.fasterxml.jackson.module.scala.DefaultRequiredAnnotationIntrospector}
  */
-@deprecated("use com.fasterxml.jackson.module.scala.DefaultRequiredAnnotationIntrospector")
+@deprecated("use com.fasterxml.jackson.module.scala.DefaultRequiredAnnotationIntrospector", "2.12.1")
 object DefaultRequiredAnnotationIntrospector extends NopAnnotationIntrospector {
   override def hasRequiredMarker(m: AnnotatedMember) =
     com.fasterxml.jackson.module.scala.DefaultRequiredAnnotationIntrospector.hasRequiredMarker(m)
@@ -15,5 +15,5 @@ object DefaultRequiredAnnotationIntrospector extends NopAnnotationIntrospector {
 /**
  * @deprecated use {@link com.fasterxml.jackson.module.scala.RequiredPropertiesSchemaModule}
  */
-@deprecated("use com.fasterxml.jackson.module.scala.RequiredPropertiesSchemaModule")
+@deprecated("use com.fasterxml.jackson.module.scala.RequiredPropertiesSchemaModule", "2.12.1")
 trait RequiredPropertiesSchemaModule extends com.fasterxml.jackson.module.scala.RequiredPropertiesSchemaModule
