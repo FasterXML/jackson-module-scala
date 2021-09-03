58026621d3f512ffe3fb0ba03c8b38e87c8af9d7
PJ Fanning <pjfanning@users.noreply.github.com>
2021-09-03 21:44:45 +0100

    add ScalaObjectDeserializerModule

---
+++ src/main/scala/com/fasterxml/jackson/module/scala/deser/ScalaObjectDeserializerModule.scala
@@ -0,0 +1,31 @@
+package com.fasterxml.jackson.module.scala.deser
+
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.deser.Deserializers
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer
+import com.fasterxml.jackson.databind._
+import com.fasterxml.jackson.module.scala.JacksonModule
+import com.fasterxml.jackson.module.scala.util.ClassW
+
+import scala.languageFeature.postfixOps
+
+private class ScalaObjectDeserializer(clazz: Class[_]) extends StdDeserializer[Any](classOf[Any]) {
+  override def deserialize(p: JsonParser, ctxt: DeserializationContext): Any = {
+    clazz.getDeclaredFields.find(_.getName == "MODULE$").map(_.get(null)).getOrElse(null)
+  }
+}
+
+private object ScalaObjectDeserializerResolver extends Deserializers.Base {
+  override def findBeanDeserializer(javaType: JavaType, config: DeserializationConfig, beanDesc: BeanDescription): JsonDeserializer[_] = {
+    val clazz = javaType.getRawClass
+    if (ClassW(clazz).isScalaObject)
+      new ScalaObjectDeserializer(clazz)
+    else null
+  }
+}
+
+trait ScalaObjectDeserializerModule extends JacksonModule {
+  this += { _ addDeserializers ScalaObjectDeserializerResolver }
+}
+
+object ScalaObjectDeserializerModule extends ScalaObjectDeserializerModule
