6578b47830d9fa1bbea28c04f7f1ae49a736518c
PJ Fanning <pjfanning@users.noreply.github.com>
2021-09-16 08:56:38 +0100

    rework TastyUtil

--- src/test/scala/com/fasterxml/jackson/module/scala/QuxClassTagExtensionsTest.scala
+++ src/test/scala/com/fasterxml/jackson/module/scala/QuxClassTagExtensionsTest.scala
@@ -4,14 +4,9 @@ import com.fasterxml.jackson.databind.json.JsonMapper
 
 class QuxClassTagExtensionsTest extends BaseSpec {
   "An ObjectMapper with the ClassTagExtensions mixin" should "deserialize Qux" in {
-    val javaVersion = System.getProperty("java.specification.version")
-    if (javaVersion == "1.8") {
-      //skip - this test fails on Java 1.8 - see https://github.com/FasterXML/jackson-module-scala/issues/542
-    } else {
-      val objectMapper = JsonMapper
-        .builder().addModule(DefaultScalaModule).build() :: ClassTagExtensions
-      val qux = objectMapper.readValue[Qux]("""{"qux": {"num": "3"}}""")
-      qux.qux.num shouldEqual 3
-    }
+    val objectMapper = JsonMapper
+      .builder().addModule(DefaultScalaModule).build() :: ClassTagExtensions
+    val qux = objectMapper.readValue[Qux]("""{"qux": {"num": "3"}}""")
+    qux.qux.num shouldEqual 3
   }
 }
