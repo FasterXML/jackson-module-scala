b4486c99f4f3ef8db38051a481286d5e3d667f00
PJ Fanning <pjfanning@users.noreply.github.com>
2021-09-29 12:25:29 +0100

    Update JsonPropertyTest.scala

--- src/test/scala/com/fasterxml/jackson/module/scala/deser/JsonPropertyTest.scala
+++ src/test/scala/com/fasterxml/jackson/module/scala/deser/JsonPropertyTest.scala
@@ -3,30 +3,51 @@ package com.fasterxml.jackson.module.scala.deser
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.json.JsonMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
-import com.fasterxml.jackson.module.scala.deser.JsonPropertyTest.Sample1
 
 //https://github.com/FasterXML/jackson-module-scala/issues/224
 object JsonPropertyTest {
+  //this version causes problems in jackson3 - deserialization fails - Sample2 is safer
   class Sample1 {
     @JsonProperty("v") private var value: Int = 0
 
     def getValue: Int = value
     def setValue(value: Int): Unit  = {this.value = value}
   }
+
+  //this version causes problems in jackson3 - deserialization fails - Sample2 is safer
+  class Sample1a {
+    @JsonProperty("v") var value: Int = 0
+  }
+
+  class Sample2 {
+    private var value: Int = 0
+
+    @JsonProperty("v") def getValue: Int = value
+    @JsonProperty("v") def setValue(value: Int): Unit  = {this.value = value}
+  }
 }
 
 class JsonPropertyTest extends DeserializerTest {
+  import JsonPropertyTest._
   def module: DefaultScalaModule.type = DefaultScalaModule
 
   "ObjectMapper deserialization" should "support JsonProperty annotation" in {
     val json = """{"v": 1}"""
-    val token = deserialize(json, classOf[Sample1])
-    token.getValue shouldEqual 1
+    val token1 = deserialize(json, classOf[Sample1])
+    token1.getValue shouldEqual 1
+    val token1a = deserialize(json, classOf[Sample1a])
+    token1a.value shouldEqual 1
+    val token2 = deserialize(json, classOf[Sample2])
+    token2.getValue shouldEqual 1
   }
   it should "support JsonProperty annotation (without scala module)" in {
     val mapper = JsonMapper.builder().build()
     val json = """{"v": 1}"""
-    val token = mapper.readValue(json, classOf[Sample1])
-    token.getValue shouldEqual 1
+    val token1 = mapper.readValue(json, classOf[Sample1])
+    token1.getValue shouldEqual 1
+    val token1a = mapper.readValue(json, classOf[Sample1a])
+    token1a.value shouldEqual 1
+    val token2 = mapper.readValue(json, classOf[Sample2])
+    token2.getValue shouldEqual 1
   }
 }
