06d5de4360687cf5bd8a91c1c45d2614970155b3
PJ Fanning <pjfanning@users.noreply.github.com>
2021-09-29 10:17:22 +0100

    Update JsonPropertyTest.scala

--- src/test/scala/com/fasterxml/jackson/module/scala/deser/JsonPropertyTest.scala
+++ src/test/scala/com/fasterxml/jackson/module/scala/deser/JsonPropertyTest.scala
@@ -1,6 +1,7 @@
 package com.fasterxml.jackson.module.scala.deser
 
 import com.fasterxml.jackson.annotation.{JsonIgnoreProperties, JsonProperty}
+import com.fasterxml.jackson.databind.json.JsonMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.fasterxml.jackson.module.scala.deser.JsonPropertyTest.Sample1
 
@@ -21,6 +22,12 @@ class JsonPropertyTest extends DeserializerTest {
   "ObjectMapper deserialization" should "support JsonProperty annotation" in {
     val json = """{"v": 1}"""
     val token = deserialize(json, classOf[Sample1])
+    token.getValue shouldEqual 1
+  }
+  it should "support JsonProperty annotation (without scala module)" in {
+    val mapper = JsonMapper.builder().build()
+    val json = """{"v": 1}"""
+    val token = mapper.readValue(json, classOf[Sample1])
     token.getValue shouldEqual 1
   }
 }
