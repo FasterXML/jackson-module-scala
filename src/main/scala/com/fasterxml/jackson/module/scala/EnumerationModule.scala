7752b2258fdcc5e449cde9055a9afdad049fe38d
PJ Fanning <pjfanning@users.noreply.github.com>
2022-03-10 15:48:24 +0100

    Update EnumerationModule.scala

--- src/main/scala/com/fasterxml/jackson/module/scala/EnumerationModule.scala
+++ src/main/scala/com/fasterxml/jackson/module/scala/EnumerationModule.scala
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.module.scala.deser.EnumerationDeserializerModule
 import com.fasterxml.jackson.module.scala.ser.EnumerationSerializerModule
 
 /**
- * Adds serialization and deserization support for Scala Enumerations.
+ * Adds serialization and deserialization support for Scala Enumerations.
  */
 trait EnumerationModule extends EnumerationSerializerModule with EnumerationDeserializerModule {
   override def getInitializers(config: ScalaModule.Config): Seq[SetupContext => Unit] = {
