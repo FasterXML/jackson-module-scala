ed785c7268bd51970f0a1b8fe2a9b6c48a0273aa
PJ Fanning <pjfanning@users.noreply.github.com>
2022-03-31 15:24:46 +0200

    refactor scala number deserialization to reuse java equivalents (#574)

--- src/main/scala/com/fasterxml/jackson/module/scala/deser/ScalaNumberDeserializersModule.scala
+++ src/main/scala/com/fasterxml/jackson/module/scala/deser/ScalaNumberDeserializersModule.scala
@@ -2,83 +2,38 @@ package com.fasterxml.jackson
 package module.scala
 package deser
 
-import com.fasterxml.jackson.core.JsonToken.{START_ARRAY, VALUE_NUMBER_FLOAT, VALUE_NUMBER_INT, VALUE_STRING}
-import com.fasterxml.jackson.core.io.BigDecimalParser
-import com.fasterxml.jackson.core.{JsonParser, JsonToken}
-import com.fasterxml.jackson.databind.DeserializationFeature.UNWRAP_SINGLE_VALUE_ARRAYS
-import com.fasterxml.jackson.databind.JacksonModule.SetupContext
-import com.fasterxml.jackson.databind._
+import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.databind.deser.Deserializers
 import com.fasterxml.jackson.databind.deser.std.StdScalarDeserializer
-import com.fasterxml.jackson.module.scala.JacksonModule.InitializerBuilder
-import com.fasterxml.jackson.module.scala.{JacksonModule => JacksonScalaModule}
+import com.fasterxml.jackson.databind._
+import com.fasterxml.jackson.databind.deser.std.NumberDeserializers.{
+  BigDecimalDeserializer => JavaBigDecimalDeserializer, BigIntegerDeserializer
+}
 
-import scala.reflect.{ClassTag, classTag}
-
-private abstract class BigNumberDeserializer[T >: Null : ClassTag](creator: (String) => T)
-  extends StdScalarDeserializer[T](classTag[T].runtimeClass)
-{
-  override def deserialize(jp: JsonParser, ctxt: DeserializationContext): T = {
-    val t = jp.currentToken()
-    t match {
-      case VALUE_NUMBER_INT | VALUE_NUMBER_FLOAT => creator(jp.getText.trim)
-      case VALUE_STRING =>
-        val text = jp.getText.trim
-        if (text.isEmpty) None.orNull else try {
-          creator(text)
-        }
-        catch {
-          case _: IllegalArgumentException => throw ctxt.weirdStringException(text, _valueClass, "not a valid representation")
-        }
-      case START_ARRAY if ctxt.isEnabled(UNWRAP_SINGLE_VALUE_ARRAYS) =>
-        jp.nextToken()
-        val value = deserialize(jp, ctxt)
-        if (jp.nextToken() != JsonToken.END_ARRAY) {
-          throw ctxt.wrongTokenException(jp, ctxt.getContextualType,
-            JsonToken.END_ARRAY, "Attempted to unwrap array for single value but there was more than a single value in the array")
-        }
-        value
-      case _ =>
-        ctxt.handleUnexpectedToken(_valueType, jp).asInstanceOf[T]
-    }
+private object BigDecimalDeserializer extends StdScalarDeserializer[BigDecimal](classOf[BigDecimal]) {
+  override def deserialize(p: JsonParser, ctxt: DeserializationContext): BigDecimal = {
+    JavaBigDecimalDeserializer.instance.deserialize(p, ctxt)
   }
 }
 
-private object ScalaBigDecimalParser {
-  def fromString(str: String): BigDecimal = BigDecimalParser.parse(str)
+private object BigIntDeserializer extends StdScalarDeserializer[BigInt](classOf[BigInt]) {
+  override def deserialize(p: JsonParser, ctxt: DeserializationContext): BigInt = {
+    BigIntegerDeserializer.instance.deserialize(p, ctxt)
+  }
 }
 
-private object BigDecimalDeserializer extends BigNumberDeserializer(ScalaBigDecimalParser.fromString)
+private object NumberDeserializers extends Deserializers.Base {
+  private val BigDecimalClass = classOf[BigDecimal]
+  private val BigIntClass = classOf[BigInt]
 
-private object BigIntDeserializer extends BigNumberDeserializer(BigInt.apply)
-
-private class NumberDeserializers(config: ScalaModule.Config) extends Deserializers.Base
-{
-  private val BigDecimalClass = BigDecimalDeserializer.handledType()
-  private val BigIntClass = BigIntDeserializer.handledType()
-
-  override def findBeanDeserializer(tpe: JavaType, deserializationConfig: DeserializationConfig, beanDesc: BeanDescription): ValueDeserializer[_] =
+  override def findBeanDeserializer(tpe: JavaType, config: DeserializationConfig, beanDesc: BeanDescription): JsonDeserializer[_] =
     tpe.getRawClass match {
       case BigDecimalClass => BigDecimalDeserializer
       case BigIntClass => BigIntDeserializer
       case _ => None.orNull
     }
-
-  override def hasDeserializerFor(deserializationConfig: DeserializationConfig, valueType: Class[_]): Boolean = {
-    valueType match {
-      case BigDecimalClass => true
-      case BigIntClass => true
-      case _ => false
-    }
-  }
 }
 
-trait ScalaNumberDeserializersModule extends JacksonScalaModule {
-  override def getInitializers(config: ScalaModule.Config): Seq[SetupContext => Unit] = {
-    val builder = new InitializerBuilder()
-    builder += new NumberDeserializers(config)
-    builder.build()
-  }
+trait ScalaNumberDeserializersModule extends JacksonModule {
+  this += NumberDeserializers
 }
-
-object ScalaNumberDeserializersModule extends ScalaNumberDeserializersModule
