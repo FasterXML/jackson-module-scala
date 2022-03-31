645bb96a27903ddcc37839ffab34a11f8c0dfd56
PJ Fanning <pjfanning@users.noreply.github.com>
2022-03-31 15:41:23 +0200

    Update ScalaNumberDeserializersModule.scala

--- src/main/scala/com/fasterxml/jackson/module/scala/deser/ScalaNumberDeserializersModule.scala
+++ src/main/scala/com/fasterxml/jackson/module/scala/deser/ScalaNumberDeserializersModule.scala
@@ -3,12 +3,16 @@ package module.scala
 package deser
 
 import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.JacksonModule.SetupContext
 import com.fasterxml.jackson.databind.deser.Deserializers
 import com.fasterxml.jackson.databind.deser.std.StdScalarDeserializer
 import com.fasterxml.jackson.databind._
-import com.fasterxml.jackson.databind.deser.std.NumberDeserializers.{
-  BigDecimalDeserializer => JavaBigDecimalDeserializer, BigIntegerDeserializer
+import com.fasterxml.jackson.databind.deser.jdk.NumberDeserializers.{
+  BigDecimalDeserializer => JavaBigDecimalDeserializer,
+  BigIntegerDeserializer
 }
+import com.fasterxml.jackson.module.scala.JacksonModule.InitializerBuilder
+import com.fasterxml.jackson.module.scala.{JacksonModule => JacksonScalaModule}
 
 private object BigDecimalDeserializer extends StdScalarDeserializer[BigDecimal](classOf[BigDecimal]) {
   override def deserialize(p: JsonParser, ctxt: DeserializationContext): BigDecimal = {
@@ -22,18 +26,35 @@ private object BigIntDeserializer extends StdScalarDeserializer[BigInt](classOf[
   }
 }
 
-private object NumberDeserializers extends Deserializers.Base {
+private object NumberDeserializers {
   private val BigDecimalClass = classOf[BigDecimal]
   private val BigIntClass = classOf[BigInt]
+}
 
-  override def findBeanDeserializer(tpe: JavaType, config: DeserializationConfig, beanDesc: BeanDescription): JsonDeserializer[_] =
+private class NumberDeserializers(config: ScalaModule.Config) extends Deserializers.Base {
+
+  override def findBeanDeserializer(tpe: JavaType, config: DeserializationConfig, beanDesc: BeanDescription): ValueDeserializer[_] =
     tpe.getRawClass match {
-      case BigDecimalClass => BigDecimalDeserializer
-      case BigIntClass => BigIntDeserializer
+      case NumberDeserializers.BigDecimalClass => BigDecimalDeserializer
+      case NumberDeserializers.BigIntClass => BigIntDeserializer
       case _ => None.orNull
     }
+
+  override def hasDeserializerFor(deserializationConfig: DeserializationConfig, valueType: Class[_]): Boolean = {
+    valueType match {
+      case NumberDeserializers.BigDecimalClass => true
+      case NumberDeserializers.BigIntClass => true
+      case _ => false
+    }
+  }
 }
 
-trait ScalaNumberDeserializersModule extends JacksonModule {
-  this += NumberDeserializers
+trait ScalaNumberDeserializersModule extends JacksonScalaModule {
+  override def getInitializers(config: ScalaModule.Config): Seq[SetupContext => Unit] = {
+    val builder = new InitializerBuilder()
+    builder += new NumberDeserializers(config)
+    builder.build()
+  }
 }
+
+object ScalaNumberDeserializersModule extends ScalaNumberDeserializersModule
