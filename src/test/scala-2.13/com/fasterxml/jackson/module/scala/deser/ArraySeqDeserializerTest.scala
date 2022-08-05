aaf6fc1a8cf93f76022b89af700dc33066dbc249
PJ Fanning <pjfanning@users.noreply.github.com>
2022-08-05 13:08:36 +0100

    mutable ArraySeq works

--- src/test/scala-2.13/com/fasterxml/jackson/module/scala/deser/ArraySeqDeserializerTest.scala
+++ src/test/scala-2.13/com/fasterxml/jackson/module/scala/deser/ArraySeqDeserializerTest.scala
@@ -5,19 +5,26 @@ import com.fasterxml.jackson.databind.json.JsonMapper
 import com.fasterxml.jackson.module.scala.{DefaultScalaModule, JacksonModule}
 
 import java.nio.charset.StandardCharsets
-import scala.collection.immutable.ArraySeq
+import scala.collection.immutable
+import scala.collection.mutable
 
 class ArraySeqSerializerTest extends DeserializerTest {
 
   lazy val module: JacksonModule = DefaultScalaModule
+  val arraySize = 1000
+  val obj = (1 to arraySize).map(i => ((i * 1498724053) & 0x1) == 0).toArray
+  val jsonString = obj.mkString("[", ",", "]")
+  val jsonBytes = jsonString.getBytes(StandardCharsets.UTF_8)
 
-  "An ObjectMapper with the SeqDeserializer" should "handle ArraySeq of booleans" in {
+  "An ObjectMapper with the SeqDeserializer" should "handle immutable ArraySeq of booleans" in {
     val mapper = JsonMapper.builder().addModule(DefaultScalaModule).build()
-    val size = 1000
-    val obj = (1 to size).map(i => ((i * 1498724053) & 0x1) == 0).toArray
-    val jsonString = obj.mkString("[", ",", "]")
-    val jsonBytes = jsonString.getBytes(StandardCharsets.UTF_8)
-    val seq = mapper.readValue(jsonBytes, new TypeReference[ArraySeq[Boolean]] {})
-    seq should have size size
+    val seq = mapper.readValue(jsonBytes, new TypeReference[immutable.ArraySeq[Boolean]] {})
+    seq should have size arraySize
+  }
+
+  it should "handle mutable ArraySeq of booleans" in {
+    val mapper = JsonMapper.builder().addModule(DefaultScalaModule).build()
+    val seq = mapper.readValue(jsonBytes, new TypeReference[mutable.ArraySeq[Boolean]] {})
+    seq should have size arraySize
   }
 }
