67924a98e43b999fe6f539a7945e4158aa918a5c
PJ Fanning <pjfanning@users.noreply.github.com>
2022-08-05 14:15:25 +0100

    add test

---
+++ src/test/scala-3.0/tools/jackson/module/scala/deser/IArrayDeserializerTest.scala
@@ -0,0 +1,18 @@
+package tools.jackson.module.scala.deser
+
+import tools.jackson.core.`type`.TypeReference
+import tools.jackson.module.scala.{DefaultScalaModule, JacksonModule}
+
+class IArrayDeserializerTest extends DeserializerTest {
+
+  lazy val module: JacksonModule = DefaultScalaModule
+  val listJson =  "[1,2,3,4,5,6]"
+  val listScala: Range.Inclusive = 1 to 6
+
+  "An ObjectMapper with the SeqDeserializer" should "deserialize a list into an immutable Array" in {
+    val result = deserialize(listJson, new TypeReference[IArray[Int]]{})
+    result shouldBe an[IArray[Int]]
+    result should equal (listScala)
+  }
+
+}
