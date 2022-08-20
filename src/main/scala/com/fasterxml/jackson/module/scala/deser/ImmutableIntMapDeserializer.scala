9b253b39b62129c55d1973d7dbfe769189f65f90
PJ Fanning <pjfanning@users.noreply.github.com>
2022-08-20 15:36:20 +0100

    wip

--- src/main/scala/com/fasterxml/jackson/module/scala/deser/ImmutableIntMapDeserializer.scala
+++ src/main/scala/com/fasterxml/jackson/module/scala/deser/ImmutableIntMapDeserializer.scala
@@ -1,12 +1,15 @@
 package com.fasterxml.jackson.module.scala.deser
 
 import com.fasterxml.jackson.core.JsonParser
-import com.fasterxml.jackson.databind.DeserializationContext
-import com.fasterxml.jackson.databind.deser.std.StdDeserializer
-import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.`type`.MapLikeType
+import com.fasterxml.jackson.databind.deser.{ContextualDeserializer, Deserializers, ValueInstantiator}
+import com.fasterxml.jackson.databind.deser.std.{ContainerDeserializerBase, MapDeserializer, StdValueInstantiator}
+import com.fasterxml.jackson.databind.jsontype.TypeDeserializer
+import com.fasterxml.jackson.databind._
 import com.fasterxml.jackson.module.scala.{DefaultScalaModule, IteratorModule}
 
-import scala.collection.immutable
+import scala.collection.immutable.IntMap
+import scala.collection.{Map, mutable}
 import scala.languageFeature.postfixOps
 
 /**
@@ -15,15 +18,83 @@ import scala.languageFeature.postfixOps
  *
  * @since 2.14.0
  */
-object ImmutableIntMapDeserializer extends StdDeserializer[immutable.IntMap[_]](classOf[immutable.IntMap[_]]) {
-  override def deserialize(p: JsonParser, ctxt: DeserializationContext): immutable.IntMap[_] = {
-    val node: JsonNode = p.getCodec.readTree(p)
-    val iter = node.fields()
-    var map = immutable.IntMap[String]()
-    while (iter.hasNext) {
-      val entry = iter.next()
-      map += (entry.getKey.toInt, entry.getValue.asText())
+class ImmutableIntMapDeserializerResolver extends Deserializers.Base {
+  override def findMapLikeDeserializer(theType: MapLikeType,
+                                       config: DeserializationConfig,
+                                       beanDesc: BeanDescription,
+                                       keyDeserializer: KeyDeserializer,
+                                       elementTypeDeserializer: TypeDeserializer,
+                                       elementDeserializer: JsonDeserializer[_]): JsonDeserializer[_] = {
+    if (!classOf[IntMap[_]].isAssignableFrom(theType.getRawClass)) None.orNull
+    else {
+      new IntMapDeserializer(theType, new Instantiator(config, theType), keyDeserializer, elementDeserializer, elementTypeDeserializer)
     }
-    map
+  }
+
+  private class Instantiator(config: DeserializationConfig, mapType: MapLikeType) extends StdValueInstantiator(config, mapType) {
+    override def canCreateUsingDefault = true
+
+    override def createUsingDefault(ctxt: DeserializationContext) =
+      new BuilderWrapper[AnyRef](IntMap.newBuilder[AnyRef])
+  }
+
+  private class BuilderWrapper[V >: AnyRef](val builder: mutable.Builder[Int, V]) extends java.util.AbstractMap[Int, V] {
+    private var baseMap: Map[Any, V] = Map.empty
+
+    override def put(k: Int, v: V): V = {
+      builder += ((k, v));
+      v
+    }
+
+    // Used by the deserializer when using readerForUpdating
+    override def get(key: Any): V = baseMap.get(key).orNull
+
+    // Isn't used by the deserializer
+    override def entrySet(): java.util.Set[java.util.Map.Entry[Int, V]] = throw new UnsupportedOperationException
+
+    def setInitialValue(init: Map[Int, V]): Unit = {
+      init.foreach(Function.tupled(put))
+      baseMap = init.asInstanceOf[Map[Any, V]]
+    }
+  }
+
+  private class IntMapDeserializer[V](mapType: MapLikeType, containerDeserializer: MapDeserializer)
+    extends ContainerDeserializerBase[IntMap[V]](mapType) with ContextualDeserializer {
+
+    def this(mapType: MapLikeType, valueInstantiator: ValueInstantiator, keyDeser: KeyDeserializer, valueDeser: JsonDeserializer[_], valueTypeDeser: TypeDeserializer) = {
+      this(mapType, new MapDeserializer(mapType, valueInstantiator, keyDeser, valueDeser.asInstanceOf[JsonDeserializer[AnyRef]], valueTypeDeser))
+    }
+
+    override def getContentType: JavaType = containerDeserializer.getContentType
+
+    override def getContentDeserializer: JsonDeserializer[AnyRef] = containerDeserializer.getContentDeserializer
+
+    override def createContextual(ctxt: DeserializationContext, property: BeanProperty): JsonDeserializer[_] = {
+      val newDelegate = containerDeserializer.createContextual(ctxt, property).asInstanceOf[MapDeserializer]
+      new IntMapDeserializer(mapType, newDelegate)
+    }
+
+    override def deserialize(jp: JsonParser, ctxt: DeserializationContext): IntMap[V] = {
+      containerDeserializer.deserialize(jp, ctxt) match {
+        case wrapper: BuilderWrapper[_] => wrapper.builder.result().asInstanceOf[IntMap[V]]
+      }
+    }
+
+    override def deserialize(jp: JsonParser, ctxt: DeserializationContext, intoValue: IntMap[V]): IntMap[V] = {
+      val bw = newBuilderWrapper(ctxt)
+      bw.setInitialValue(intoValue.asInstanceOf[Map[Int, AnyRef]])
+      containerDeserializer.deserialize(jp, ctxt, bw) match {
+        case wrapper: BuilderWrapper[_] => wrapper.builder.result().asInstanceOf[IntMap[V]]
+      }
+    }
+
+    override def getEmptyValue(ctxt: DeserializationContext): Object = {
+      val bw = newBuilderWrapper(ctxt)
+      bw.builder.result().asInstanceOf[Object]
+    }
+
+    private def newBuilderWrapper(ctxt: DeserializationContext): BuilderWrapper[AnyRef] = {
+      containerDeserializer.getValueInstantiator.createUsingDefault(ctxt).asInstanceOf[BuilderWrapper[AnyRef]]
+    }
   }
 }
