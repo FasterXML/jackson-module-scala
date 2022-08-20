233b5c795dc6be16bf648582e2139c259fdc532d
PJ Fanning <pjfanning@users.noreply.github.com>
2022-08-20 19:20:28 +0100

    Update ScalaTypeModifier.scala

--- src/main/scala/com/fasterxml/jackson/module/scala/modifiers/ScalaTypeModifier.scala
+++ src/main/scala/com/fasterxml/jackson/module/scala/modifiers/ScalaTypeModifier.scala
@@ -1,18 +1,23 @@
 package com.fasterxml.jackson.module.scala.modifiers
 
-import java.lang.reflect.Type
-
 import com.fasterxml.jackson.databind.JavaType
 import com.fasterxml.jackson.databind.`type`._
 import com.fasterxml.jackson.module.scala.JacksonModule
 
+import java.lang.reflect.Type
 import scala.collection._
+import scala.collection.immutable.IntMap
 
 class ScalaTypeModifier extends TypeModifier {
 
   private val optionClass = classOf[Option[_]]
   private val eitherClass = classOf[Either[_, _]]
   private val mapClass = classOf[Map[_, _]]
+  private val intClass = classOf[Int]
+  private val intMapClass = classOf[IntMap[_]]
+  private val longClass = classOf[Long]
+  private val immutableLongMapClass = classOf[immutable.LongMap[_]]
+  private val mutableLongMapClass = classOf[mutable.LongMap[_]]
   private val iterableOnceClass = classOf[IterableOnce[_]]
 
   override def modifyType(javaType: JavaType,
@@ -27,7 +32,13 @@ class ScalaTypeModifier extends TypeModifier {
         case _ => ReferenceType.upgradeFrom(javaType, javaType.containedTypeOrUnknown(0))
       }
     } else if (javaType.isTypeOrSubTypeOf(mapClass)) {
-      MapLikeType.upgradeFrom(javaType, javaType.containedTypeOrUnknown(0), javaType.containedTypeOrUnknown(1))
+      if (javaType.isTypeOrSubTypeOf(intMapClass)) {
+        MapLikeType.upgradeFrom(javaType, typeFactory.constructType(intClass), javaType.containedTypeOrUnknown(0))
+      } else if (javaType.isTypeOrSubTypeOf(immutableLongMapClass) || javaType.isTypeOrSubTypeOf(mutableLongMapClass)) {
+        MapLikeType.upgradeFrom(javaType, typeFactory.constructType(longClass), javaType.containedTypeOrUnknown(0))
+      } else {
+        MapLikeType.upgradeFrom(javaType, javaType.containedTypeOrUnknown(0), javaType.containedTypeOrUnknown(1))
+      }
     } else if (javaType.isTypeOrSubTypeOf(iterableOnceClass)) {
       CollectionLikeType.upgradeFrom(javaType, javaType.containedTypeOrUnknown(0))
     } else if (javaType.isTypeOrSubTypeOf(eitherClass)) {
