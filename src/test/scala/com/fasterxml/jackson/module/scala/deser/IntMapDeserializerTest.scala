e21c73cb2ea4a16db141f2459c5475c8cb9592fa
PJ Fanning <pjfanning@users.noreply.github.com>
2022-08-20 19:30:28 +0100

    Update IntMapDeserializerTest.scala

--- src/test/scala/com/fasterxml/jackson/module/scala/deser/IntMapDeserializerTest.scala
+++ src/test/scala/com/fasterxml/jackson/module/scala/deser/IntMapDeserializerTest.scala
@@ -2,8 +2,14 @@ package com.fasterxml.jackson.module.scala.deser
 
 import com.fasterxml.jackson.core.`type`.TypeReference
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.module.scala.deser.IntMapDeserializerTest.IntMapWrapper
+import com.fasterxml.jackson.module.scala.introspect.ScalaAnnotationIntrospectorModule
 
 import scala.collection.immutable.IntMap
+
+object IntMapDeserializerTest {
+  case class IntMapWrapper(values: IntMap[Long])
+}
 
 class IntMapDeserializerTest extends DeserializerTest {
 
@@ -27,6 +33,33 @@ class IntMapDeserializerTest extends DeserializerTest {
 
     val json = mapper.writeValueAsString(map)
     val read = mapper.readValue(json, new TypeReference[IntMap[Long]] {})
+
+    read shouldEqual map
+    // next line fails due to type erasure (values are ints and won't cast to longs)
+    //read.values.sum shouldEqual map.values.sum
+  }
+
+  it should "deserialize IntMapWrapper" in {
+    val map = IntMap(1 -> 100L, 2 -> 200L)
+    val instance = IntMapWrapper(map)
+
+    val mapper = newBuilder.addModule(new IntMapDeserializerModule() {}).build()
+
+    val json = mapper.writeValueAsString(instance)
+    val read = mapper.readValue(json, classOf[IntMapWrapper])
+
+    read shouldEqual instance
+    // next line fails due to type erasure (values are ints and won't cast to longs)
+    //read.values.values.sum shouldEqual map.values.sum
+  }
+
+  it should "deserialize IntMap (bigint value)" in {
+    val map = IntMap(1 -> 100L, 2 -> 200L)
+
+    val mapper = newBuilder.addModule(new IntMapDeserializerModule() {}).build()
+
+    val json = mapper.writeValueAsString(map)
+    val read = mapper.readValue(json, new TypeReference[IntMap[BigInt]] {})
 
     read shouldEqual map
     read.values.sum shouldEqual map.values.sum
