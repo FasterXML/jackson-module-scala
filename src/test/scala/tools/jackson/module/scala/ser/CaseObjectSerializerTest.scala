5dfc6ddd593a69bc60411f3653051605e571bff6
PJ Fanning <pjfanning@users.noreply.github.com>
2022-09-01 00:54:51 +0100

    Update CaseObjectSerializerTest.scala

--- src/test/scala/tools/jackson/module/scala/ser/CaseObjectSerializerTest.scala
+++ src/test/scala/tools/jackson/module/scala/ser/CaseObjectSerializerTest.scala
@@ -1,6 +1,10 @@
 package tools.jackson.module.scala.ser
 
+import com.fasterxml.jackson.annotation.{JsonAutoDetect, PropertyAccessor}
+import tools.jackson.databind.introspect.VisibilityChecker
 import tools.jackson.module.scala.DefaultScalaModule
+
+import scala.compat.java8.FunctionConverters.asJavaUnaryOperator
 
 case object CaseObjectExample {
   val field1: String = "test"
@@ -9,12 +13,41 @@ case object CaseObjectExample {
 
 class CaseObjectSerializerTest extends SerializerTest {
 
+  case object Foo {
+    val field: String = "bar"
+  }
+
   def module = DefaultScalaModule
 
   "An ObjectMapper with the DefaultScalaModule" should "serialize a case object as a bean" in {
     serialize(CaseObjectExample) should (
        equal ("""{"field1":"test","field2":42}""") or
-       equal ("""{"field2":42,"field1":"test"}""")
+         equal ("""{"field2":42,"field1":"test"}""")
     )
+  }
+
+  it should "serialize a case object when visibility settings set" ignore {
+    val mapper = newBuilder
+      .changeDefaultVisibility(asJavaUnaryOperator((a: Any) => {
+        VisibilityChecker.defaultInstance()
+          .withFieldVisibility(JsonAutoDetect.Visibility.ANY)
+          .withGetterVisibility(JsonAutoDetect.Visibility.NONE)
+      }))
+      .build()
+    mapper.writeValueAsString(CaseObjectExample) should (
+      equal("""{"field1":"test","field2":42}""") or
+        equal("""{"field2":42,"field1":"test"}""")
+      )
+  }
+
+  it should "serialize an inner case object when visibility settings set" in {
+    val mapper = newBuilder
+      .changeDefaultVisibility(asJavaUnaryOperator((a: Any) => {
+        VisibilityChecker.defaultInstance()
+          .withFieldVisibility(JsonAutoDetect.Visibility.ANY)
+          .withGetterVisibility(JsonAutoDetect.Visibility.NONE)
+      }))
+      .build()
+    mapper.writeValueAsString(Foo) shouldEqual """{"field":"bar"}"""
   }
 }
