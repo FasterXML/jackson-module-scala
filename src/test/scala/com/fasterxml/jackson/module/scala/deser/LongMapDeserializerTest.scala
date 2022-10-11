22a1529687dd43e2fabf98b20904a6603e493d8b
PJ Fanning <pjfanning@users.noreply.github.com>
2022-10-11 21:42:29 +0100

    Update LongMapDeserializerTest.scala

--- src/test/scala/com/fasterxml/jackson/module/scala/deser/LongMapDeserializerTest.scala
+++ src/test/scala/com/fasterxml/jackson/module/scala/deser/LongMapDeserializerTest.scala
@@ -47,6 +47,54 @@ class LongMapDeserializerTest extends DeserializerTest {
     read(402) shouldBe true
   }
 
+  it should "deserialize immutable LongMap (Object values)" in {
+    val event = Event(UUID.randomUUID(), "event1")
+    val map = mutable.LongMap(0L -> false, 1L -> "true", 2L -> event)
+    val mapper = newMapper
+    val json = mapper.writeValueAsString(map)
+    val read = mapper.readValue(json, classOf[immutable.LongMap[Any]])
+    read(0) shouldBe false
+    read(1) shouldEqual "true"
+    read(2) shouldEqual Map("id" -> event.id.toString, "description" -> event.description)
+  }
+
+  it should "deserialize immutable LongMap (Object values, duplicate keys - default mode)" in {
+    val mapper = newMapper
+    val json = """{"1": 123, "2": 123, "2": 123.456}"""
+    val read = mapper.readValue(json, classOf[immutable.LongMap[Any]])
+    read(1) shouldEqual 123
+    read(2) shouldEqual 123.456
+  }
+
+  it should "deserialize immutable LongMap (Object values, duplicate keys - optional mode)" in {
+    val mapper = newMapper
+    val json = """{"1": 123, "2": 123, "2": 123.456}"""
+    val parser = new WithDupsParser(mapper.createParser(json))
+    val read = try {
+      mapper.readValue(parser, classOf[immutable.LongMap[Any]])
+    } finally {
+      parser.close()
+    }
+    read(1) shouldEqual 123
+    val expected = new java.util.ArrayList[Object]()
+    expected.add(java.lang.Integer.valueOf(123))
+    expected.add(java.lang.Double.valueOf(123.456))
+    read(2) shouldEqual expected
+  }
+
+  it should "deserialize immutable LongMap (Double values, duplicate key mode is ignored)" in {
+    val mapper = newMapper
+    val json = """{"1": "123", "2": "123", "2": "123.456"}"""
+    val parser = new WithDupsParser(mapper.createParser(json))
+    val read = try {
+      mapper.readValue(parser, new TypeReference[immutable.LongMap[String]] {})
+    } finally {
+      parser.close()
+    }
+    read(1) shouldEqual "123"
+    read(2) shouldEqual "123.456"
+  }
+
   it should "deserialize mutable LongMap" in {
     val map = mutable.LongMap(1L -> "one", 2L -> "two")
 
@@ -83,7 +131,7 @@ class LongMapDeserializerTest extends DeserializerTest {
     read(402) shouldBe true
   }
 
-  it should "deserialize IntMap (Object values)" in {
+  it should "deserialize mutable LongMap (Object values)" in {
     val event = Event(UUID.randomUUID(), "event1")
     val map = mutable.LongMap(0L -> false, 1L -> "true", 2L -> event)
     val mapper = newMapper
@@ -94,7 +142,7 @@ class LongMapDeserializerTest extends DeserializerTest {
     read(2) shouldEqual Map("id" -> event.id.toString, "description" -> event.description)
   }
 
-  it should "deserialize IntMap (Object values, duplicate keys - default mode)" in {
+  it should "deserialize mutable LongMap (Object values, duplicate keys - default mode)" in {
     val mapper = newMapper
     val json = """{"1": 123, "2": 123, "2": 123.456}"""
     val read = mapper.readValue(json, classOf[mutable.LongMap[Any]])
@@ -102,7 +150,7 @@ class LongMapDeserializerTest extends DeserializerTest {
     read(2) shouldEqual 123.456
   }
 
-  it should "deserialize IntMap (Object values, duplicate keys - optional mode)" in {
+  it should "deserialize mutable LongMap (Object values, duplicate keys - optional mode)" in {
     val mapper = newMapper
     val json = """{"1": 123, "2": 123, "2": 123.456}"""
     val parser = new WithDupsParser(mapper.createParser(json))
@@ -118,7 +166,7 @@ class LongMapDeserializerTest extends DeserializerTest {
     read(2) shouldEqual expected
   }
 
-  it should "deserialize IntMap (Double values, duplicate key mode is ignored)" in {
+  it should "deserialize mutable LongMap (Double values, duplicate key mode is ignored)" in {
     val mapper = newMapper
     val json = """{"1": "123", "2": "123", "2": "123.456"}"""
     val parser = new WithDupsParser(mapper.createParser(json))
