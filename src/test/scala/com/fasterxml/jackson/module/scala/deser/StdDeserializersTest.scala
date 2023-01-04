bc55da49847bf00d86aeaa15610f26a62c1f7558
PJ Fanning <pjfanning@users.noreply.github.com>
2023-01-04 16:18:29 +0100

    num deserialization tests

--- src/test/scala/com/fasterxml/jackson/module/scala/deser/StdDeserializersTest.scala
+++ src/test/scala/com/fasterxml/jackson/module/scala/deser/StdDeserializersTest.scala
@@ -1,5 +1,8 @@
 package com.fasterxml.jackson.module.scala.deser
 
+import com.fasterxml.jackson.core.StreamReadFeature
+import com.fasterxml.jackson.databind.json.JsonMapper
+
 
 class StdDeserializersTest extends DeserializationFixture {
 
@@ -19,7 +22,28 @@ class StdDeserializersTest extends DeserializationFixture {
 
   it should "deserialize a float into a scala BigDecimal without losing precision" in { f =>
     val manyDigits = "1.23456789012345678901234567890123456789"
-
     f.readValue(manyDigits, classOf[BigDecimal]) shouldBe BigDecimal(manyDigits)
+  }
+
+  it should "deserialize to BigDecimal a number in exponent form" in { f =>
+    Seq("1.0E+2", "1.0e+2", "1.0e2", "1.234e-234").foreach { numString =>
+      f.readValue(numString, classOf[BigDecimal]) shouldBe BigDecimal(numString)
+    }
+  }
+
+  it should "deserialize to BigDecimal a number in exponent form (Fast number parsing)" in { _ =>
+    val mapper = JsonMapper.builder()
+      .enable(StreamReadFeature.USE_FAST_DOUBLE_PARSER)
+      .enable(StreamReadFeature.USE_FAST_BIG_NUMBER_PARSER)
+      .build()
+    Seq("1.0E+2", "1.0e+2", "1.0e2", "1.234e-234").foreach { numString =>
+      mapper.readValue(numString, classOf[BigDecimal]) shouldBe BigDecimal(numString)
+    }
+  }
+
+  it should "deserialize to BigInt a number in exponent form" in { f =>
+    Seq("1.0E2", "1.0e2", "1.0e2", "10000E-2").foreach { numString =>
+      f.readValue(numString, classOf[BigInt]) shouldBe BigDecimal(numString).toBigIntExact.orNull
+    }
   }
 }
