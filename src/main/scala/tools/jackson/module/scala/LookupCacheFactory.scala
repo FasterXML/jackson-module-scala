563dc1181ab7e6aeada08d2e6b65fe98021aaf39
PJ Fanning <pjfanning@users.noreply.github.com>
2023-02-18 00:21:20 +0100

    Update LookupCacheFactory.scala

--- src/main/scala/tools/jackson/module/scala/LookupCacheFactory.scala
+++ src/main/scala/tools/jackson/module/scala/LookupCacheFactory.scala
@@ -4,6 +4,8 @@ import tools.jackson.databind.util.{LookupCache, SimpleLookupCache}
 
 /**
  * Factory for creating [[com.fasterxml.jackson.databind.util.LookupCache]] instances
+ *
+ * @since 2.14.3
  */
 trait LookupCacheFactory {
   def createLookupCache[K, V](initialEntries: Int, maxEntries: Int): LookupCache[K, V]
