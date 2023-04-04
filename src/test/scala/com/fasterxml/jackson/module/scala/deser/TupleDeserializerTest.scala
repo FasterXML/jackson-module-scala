d39015fec3eb4d7ffad1624dd480883ad8dfda52
PJ Fanning <pjfanning@users.noreply.github.com>
2023-04-04 22:39:47 +0200

    add deser test

--- src/test/scala/com/fasterxml/jackson/module/scala/deser/TupleDeserializerTest.scala
+++ src/test/scala/com/fasterxml/jackson/module/scala/deser/TupleDeserializerTest.scala
@@ -3,6 +3,7 @@ package com.fasterxml.jackson.module.scala.deser
 import com.fasterxml.jackson.annotation.JsonTypeInfo.{As, Id}
 import com.fasterxml.jackson.annotation.{JsonSubTypes, JsonTypeInfo}
 import com.fasterxml.jackson.core.`type`.TypeReference
+import com.fasterxml.jackson.module.scala.ser.TupleSerializerTest.OptionalTupleHolder
 import com.fasterxml.jackson.module.scala.{DefaultScalaModule, JacksonModule}
 
 @JsonTypeInfo(use = Id.NAME, include = As.EXTERNAL_PROPERTY, property = "type")
@@ -68,13 +69,29 @@ class TupleDeserializerTest extends DeserializerTest {
     val value = TupleContainer(TupleValueLong(1), TupleValueString("foo"))
     val json = newMapper.writeValueAsString(value)
     val result = deserialize(json, new TypeReference[TupleContainer]{})
-    result should be (value)
+    result shouldEqual value
   }
 
   it should "deserialize using type information outside of field" in {
     val value = (TupleValueLong(1), TupleValueString("foo"))
     val json = newMapper.writeValueAsString(value)
     val result = deserialize(json, new TypeReference[(TupleValueBase, TupleValueBase)]{})
-    result should be (value)
+    result shouldEqual value
+  }
+
+  it should "deserialize an OptionalTupleHolder" in {
+    val value = OptionalTupleHolder(Some(1), Some("one"))
+    val json = newMapper.writeValueAsString(value)
+    val result = deserialize(json, classOf[OptionalTupleHolder])
+    result shouldEqual value
+  }
+
+  case class OptionalTupleHolder2(tuple: (Option[Int], Option[Boolean]))
+
+  it should "deserialize an OptionalTupleHolder2 with nulls" in {
+    val value = OptionalTupleHolder2(None, None)
+    val json = newMapper.writeValueAsString(value)
+    val result = deserialize(json, classOf[OptionalTupleHolder2])
+    result shouldEqual value
   }
 }
