1d850fb846fa8c2d930057fd360a99cf34c73d84
PJ Fanning <pjfanning@users.noreply.github.com>
2023-04-04 21:59:38 +0200

    Update TupleSerializerTest.scala

--- src/test/scala/tools/jackson/module/scala/ser/TupleSerializerTest.scala
+++ src/test/scala/tools/jackson/module/scala/ser/TupleSerializerTest.scala
@@ -1,6 +1,12 @@
 package tools.jackson.module.scala.ser
 
-import tools.jackson.module.scala.JacksonModule
+import tools.jackson.databind.json.JsonMapper
+import tools.jackson.module.scala.ser.TupleSerializerTest.OptionalTupleHolder
+import tools.jackson.module.scala.{DefaultScalaModule, JacksonModule}
+
+object TupleSerializerTest {
+  case class OptionalTupleHolder(tuple: (Option[Int], Option[String]))
+}
 
 class TupleSerializerTest extends SerializerTest {
   lazy val module = new JacksonModule with TupleSerializerModule
@@ -13,5 +19,17 @@ class TupleSerializerTest extends SerializerTest {
   it should "serialize a Tuple3" in {
     val result = serialize((3.0, "A", 1))
     result should be ("""[3.0,"A",1]""")
+  }
+
+  it should "serialize an OptionalTupleHolder" in {
+    val mapper = JsonMapper.builder().addModule(DefaultScalaModule).build()
+    val result = serialize(OptionalTupleHolder(Some(1), Some("one")), mapper)
+    result should be("""{"tuple":[1,"one"]}""")
+  }
+
+  it should "serialize an OptionalTupleHolder with nulls" in {
+    val mapper = JsonMapper.builder().addModule(DefaultScalaModule).build()
+    val result = serialize(OptionalTupleHolder(None, None), mapper)
+    result should be("""{"tuple":[null,null]}""")
   }
 }
