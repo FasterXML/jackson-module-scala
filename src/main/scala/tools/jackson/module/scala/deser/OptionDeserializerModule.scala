58e5e3c79fbfb257454083b1c6a44f54b15a3f22
PJ Fanning <pjfanning@users.noreply.github.com>
2023-04-04 23:14:29 +0200

    fix issue with null handling in option deserializer

--- src/main/scala/tools/jackson/module/scala/deser/OptionDeserializerModule.scala
+++ src/main/scala/tools/jackson/module/scala/deser/OptionDeserializerModule.scala
@@ -62,7 +62,8 @@ private class OptionDeserializer(fullType: JavaType,
     val deser = valueDeserializer.getOrElse(ctxt.findContextualValueDeserializer(fullType.getContentType, beanProperty.orNull))
     val refd: AnyRef = valueTypeDeserializer match {
       case Some(vtd) => deser.deserializeWithType(p, ctxt, vtd)
-      case None => deser.deserialize(p, ctxt)
+      case None if p.getCurrentToken == JsonToken.VALUE_NULL => null
+      case _ => deser.deserialize(p, ctxt)
     }
     Option(refd)
   }
