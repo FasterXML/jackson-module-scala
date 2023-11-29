49e62348940e1b3a26df3f0b1920d422f78f446b
Marcin Belicki <marcin.belicki@protonmail.com>
2023-11-29 23:07:32 +0100

    correcting ScalaObjectDeserializerResolver

--- src/main/scala/com/fasterxml/jackson/module/scala/deser/ScalaObjectDeserializerModule.scala
+++ src/main/scala/com/fasterxml/jackson/module/scala/deser/ScalaObjectDeserializerModule.scala
@@ -1,37 +1,34 @@
 package com.fasterxml.jackson.module.scala.deser
 
 import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind._
 import com.fasterxml.jackson.databind.deser.Deserializers
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer
-import com.fasterxml.jackson.databind._
 import com.fasterxml.jackson.module.scala.JacksonModule
-import com.fasterxml.jackson.module.scala.util.ClassW
+import com.fasterxml.jackson.module.scala.util.ScalaObject
 
 import scala.languageFeature.postfixOps
-import scala.util.control.NonFatal
 
-private class ScalaObjectDeserializer(clazz: Class[_]) extends StdDeserializer[Any](classOf[Any]) {
-  override def deserialize(p: JsonParser, ctxt: DeserializationContext): Any = {
-    try {
-      clazz.getField("MODULE$").get(null)
-    } catch {
-      case NonFatal(_) => null
-    }
-  }
+private class ScalaObjectDeserializer(scalaObject: Any) extends StdDeserializer[Any](classOf[Any]) {
+  override def deserialize(p: JsonParser, ctxt: DeserializationContext): Any = scalaObject
 }
 
 private object ScalaObjectDeserializerResolver extends Deserializers.Base {
   override def findBeanDeserializer(javaType: JavaType, config: DeserializationConfig, beanDesc: BeanDescription): JsonDeserializer[_] = {
     val clazz = javaType.getRawClass
-    if (ClassW(clazz).isScalaObject)
-      new ScalaObjectDeserializer(clazz)
-    else null
+    clazz match {
+      case ScalaObject(value) => new ScalaObjectDeserializer(value)
+      case _ => null
+    }
   }
 }
 
 trait ScalaObjectDeserializerModule extends JacksonModule {
   override def getModuleName: String = "ScalaObjectDeserializerModule"
-  this += { _ addDeserializers ScalaObjectDeserializerResolver }
+
+  this += {
+    _ addDeserializers ScalaObjectDeserializerResolver
+  }
 }
 
 object ScalaObjectDeserializerModule extends ScalaObjectDeserializerModule
--- src/main/scala/com/fasterxml/jackson/module/scala/util/ScalaObject.scala
+++ src/main/scala/com/fasterxml/jackson/module/scala/util/ScalaObject.scala
@@ -1,7 +1,29 @@
 package com.fasterxml.jackson.module.scala.util
 
+import java.lang.reflect.Field
+
 object ScalaObject {
 
   private val MODULE_FIELD_NAME = "MODULE$"
 
+  private def getStaticField(field: Field): Option[Any] =
+    try Some(field.get(null))
+    catch {
+      case _: NullPointerException => None
+    }
+
+  private def moduleFieldOption(clazz: Class[_]): Option[Field] =
+    try Some(clazz.getDeclaredField(MODULE_FIELD_NAME))
+    catch {
+      case _: NoSuchFieldException => None
+    }
+
+  private def moduleFieldValue(clazz: Class[_]): Option[Any] = for {
+    moduleField <- moduleFieldOption(clazz)
+    value <- getStaticField(moduleField)
+  } yield value
+
+  def unapply(clazz: Class[_]): Option[Any] =
+    if (clazz.getSimpleName.endsWith("$")) moduleFieldValue(clazz)
+    else None
 }
