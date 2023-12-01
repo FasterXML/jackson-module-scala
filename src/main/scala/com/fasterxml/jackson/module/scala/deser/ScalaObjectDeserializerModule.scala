b71e3aaf817e25a369d3e1755e536855156d5891
PJ Fanning <pjfanning@users.noreply.github.com>
2023-12-01 13:48:00 +0100

    refactor scala object deserialization (#657)
    * refactor scala object deserialization
    
    * Update build.sbt
    
    * Update Classes.scala
    
    * refactor beanintrospector
    
    * Update build.sbt
    
    * Update CaseObjectDeserializerTest.scala

--- src/main/scala/com/fasterxml/jackson/module/scala/deser/ScalaObjectDeserializerModule.scala
+++ src/main/scala/com/fasterxml/jackson/module/scala/deser/ScalaObjectDeserializerModule.scala
@@ -10,22 +10,15 @@ import com.fasterxml.jackson.module.scala.util.ClassW
 import scala.languageFeature.postfixOps
 import scala.util.control.NonFatal
 
-private class ScalaObjectDeserializer(clazz: Class[_]) extends StdDeserializer[Any](classOf[Any]) {
-  override def deserialize(p: JsonParser, ctxt: DeserializationContext): Any = {
-    try {
-      clazz.getField("MODULE$").get(null)
-    } catch {
-      case NonFatal(_) => null
-    }
-  }
+private class ScalaObjectDeserializer(value: Any) extends StdDeserializer[Any](classOf[Any]) {
+  override def deserialize(p: JsonParser, ctxt: DeserializationContext): Any = value
 }
 
 private object ScalaObjectDeserializerResolver extends Deserializers.Base {
   override def findBeanDeserializer(javaType: JavaType, config: DeserializationConfig, beanDesc: BeanDescription): JsonDeserializer[_] = {
-    val clazz = javaType.getRawClass
-    if (ClassW(clazz).isScalaObject)
-      new ScalaObjectDeserializer(clazz)
-    else null
+    ClassW(javaType.getRawClass).getModuleField.flatMap { field =>
+      Option(field.get(null))
+    }.map(new ScalaObjectDeserializer(_)).orNull
   }
 }
 
