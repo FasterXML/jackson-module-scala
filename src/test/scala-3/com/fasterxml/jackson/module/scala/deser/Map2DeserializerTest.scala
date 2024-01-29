2b1a8a73e8de240351ff0965899753ba9e3336be
PJ Fanning <pjfanning@users.noreply.github.com>
2024-01-29 23:04:07 +0100

    TreeSeqMap deserialization (#666)
    * TreeSeqMap deserialization
    
    * try to fix tests

---
+++ src/test/scala-3/com/fasterxml/jackson/module/scala/deser/Map2DeserializerTest.scala
@@ -0,0 +1,19 @@
+package com.fasterxml.jackson.module.scala.deser
+
+import com.fasterxml.jackson.core.`type`.TypeReference
+import com.fasterxml.jackson.module.scala.{DefaultScalaModule, JacksonModule}
+
+import scala.collection.immutable
+
+class Map2DeserializerTest extends DeserializerTest {
+
+  lazy val module: JacksonModule = DefaultScalaModule
+
+  "An ObjectMapper with the SortedMapDeserializerModule" should "deserialize an object into an TreeSeqMap" in {
+    val result = deserialize(mapJson, new TypeReference[immutable.TreeSeqMap[String,String]]{})
+    result should equal (mapScala)
+  }
+
+  private val mapJson =  """{ "one": "1", "two": "2" }"""
+  private val mapScala = collection.SortedMap("one"->"1","two"->"2")
+}
