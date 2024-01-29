7d6dff2b8d202ca6c52bca25d39760a41e5ec4c0
PJ Fanning <pjfanning@users.noreply.github.com>
2024-01-30 00:34:35 +0100

    add map serializer tests

---
+++ src/test/scala-3/tools/jackson/module/scala/ser/Map2SerializerTest.scala
@@ -0,0 +1,21 @@
+package tools.jackson.module.scala.ser
+
+import tools.jackson.module.scala.{DefaultScalaModule, JacksonModule}
+
+import scala.collection.immutable.TreeSeqMap
+import scala.collection.mutable
+
+class Map2SerializerTest extends SerializerTest {
+
+  lazy val module: JacksonModule = DefaultScalaModule
+
+  "MapSerializerModule" should "serialize a TreeSeqMap" in {
+    val result = serialize(TreeSeqMap("a" -> 1, "b" -> "two", "c" -> false))
+    result shouldEqual """{"a":1,"b":"two","c":false}"""
+  }
+
+  it should "serialize a CollisionProofHashMap" in {
+    val result = serialize(mutable.CollisionProofHashMap("a" -> 1, "b" -> "two", "c" -> false))
+    result shouldEqual """{"a":1,"b":"two","c":false}"""
+  }
+}
