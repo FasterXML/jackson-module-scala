4c811cf9abd2c341597e025da4bad6071c2bc75a
PJ Fanning <pjfanning@users.noreply.github.com>
2025-01-30 19:02:29 +0100

    handle token reading issue in ScalaObjectDeserializer (#702)

--- src/main/scala/com/fasterxml/jackson/module/scala/deser/ScalaObjectDeserializerModule.scala
+++ src/main/scala/com/fasterxml/jackson/module/scala/deser/ScalaObjectDeserializerModule.scala
@@ -1,6 +1,6 @@
 package com.fasterxml.jackson.module.scala.deser
 
-import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.core.{JsonParser, JsonToken}
 import com.fasterxml.jackson.databind.deser.Deserializers
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer
 import com.fasterxml.jackson.databind._
@@ -11,7 +11,14 @@ import scala.languageFeature.postfixOps
 import scala.util.control.NonFatal
 
 private class ScalaObjectDeserializer(value: Any) extends StdDeserializer[Any](classOf[Any]) {
-  override def deserialize(p: JsonParser, ctxt: DeserializationContext): Any = value
+  override def deserialize(p: JsonParser, ctxt: DeserializationContext): Any = {
+    if (p.currentToken() != JsonToken.END_OBJECT) {
+      while (p.nextToken() != JsonToken.END_OBJECT) {
+        // consume the object
+      }
+    }
+    value
+  }
 }
 
 private object ScalaObjectDeserializerResolver extends Deserializers.Base {
