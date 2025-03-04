ab29696f0b39f390f01123bd2e7f90f75697fa60
PJ Fanning <pjfanning@users.noreply.github.com>
2025-03-04 13:18:08 +0100

    add Java DSL to help Java users

---
+++ src/main/java/tools/jackson/module/scala/javadsl/ScalaModule.java
@@ -0,0 +1,7 @@
+package tools.jackson.module.scala.javadsl;
+
+public final class ScalaModule {
+    public static tools.jackson.module.scala.ScalaModule.Builder builder() {
+        return tools.jackson.module.scala.ScalaModule$.MODULE$.builder();
+    }
+}
