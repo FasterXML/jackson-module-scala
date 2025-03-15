fa610b29c41f8c3e92cd9ed92d5bf5589736024e
PJ Fanning <pjfanning@users.noreply.github.com>
2025-03-15 15:24:56 +0100

    Update Scala3ModuleJavaDslTest.scala

--- src/test/scala-3/tools/jackson/module/scala/javadsl/Scala3ModuleJavaDslTest.scala
+++ src/test/scala-3/tools/jackson/module/scala/javadsl/Scala3ModuleJavaDslTest.scala
@@ -2,7 +2,7 @@ package tools.jackson.module.scala.javadsl
 
 import tools.jackson.module.scala.{ BaseSpec, EnumModule }
 
-class ScalaModule3Test extends BaseSpec {
+class Scala3ModuleJavaDslTest extends BaseSpec {
   "Java DSL ScalaModule" should "support EnumModule (Scala 3 only)" in {
     val module = ScalaModule.enumModule()
     module shouldBe an[EnumModule]
