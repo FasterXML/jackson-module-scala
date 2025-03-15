a3feeee4510004b444d416b772f34fad8659883a
PJ Fanning <pjfanning@users.noreply.github.com>
2025-03-15 17:17:14 +0100

    Java DSL (#714)
    * Java DSL
    
    * Update ScalaModule.java
    
    * add tests
    
    * Update ScalaModule.java

---
+++ src/main/java/com/fasterxml/jackson/module/scala/javadsl/DefaultScalaModule.java
@@ -0,0 +1,9 @@
+package com.fasterxml.jackson.module.scala.javadsl;
+
+public final class DefaultScalaModule {
+    public static com.fasterxml.jackson.module.scala.JacksonModule getInstance() {
+        return com.fasterxml.jackson.module.scala.DefaultScalaModule$.MODULE$;
+    }
+
+    private DefaultScalaModule() { }
+}
