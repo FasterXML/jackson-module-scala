e21f1a4dbb7c38b961ea6678a2b7cadea1170c48
PJ Fanning <pjfanning@users.noreply.github.com>
2025-03-28 12:43:32 +0100

    remove 2.11 source

--- src/main/scala-2.11/tools/jackson/module/scala/introspect/LazyListSupport.scala
+++
@@ -1,9 +0,0 @@
-package tools.jackson.module.scala.introspect
-
-private[introspect] object LazyListSupport {
-  type LazyListType[T] = Stream[T]
-
-  def empty[T]: LazyListType[T] = Stream.empty[T]
-
-  def fromArray[T](array: Array[T]): LazyListType[T] = array.toStream
-}
--- src/main/scala-2.11/tools/jackson/module/scala/util/TastyUtil.scala
+++
@@ -1,6 +0,0 @@
-package tools.jackson.module.scala.util
-
-private[util] object TastyUtil {
-  // this check is not useful in Scala 2.11/2.12
-  def hasTastyFile(clz: Class[_]): Boolean = false
-}
