4278926c1c073083f0ae9229b56e52a6a1ff3601
PJ Fanning <pjfanning@users.noreply.github.com>
2025-05-24 10:41:20 +0100

    Create ServiceLoaderSpec.scala

---
+++ src/test/scala/tools/jackson/module/scala/ServiceLoaderSpec.scala
@@ -0,0 +1,16 @@
+package tools.jackson.module.scala
+
+import java.util.ServiceLoader
+
+import scala.collection.JavaConverters._
+
+class ServiceLoaderSpec extends BaseSpec {
+  "A ServiceLoader" should "be able to load DefaultScalaModule" in {
+    val loader = ServiceLoader.load(classOf[com.fasterxml.jackson.databind.JacksonModule])
+    val modules = loader.iterator().asScala.toList
+    modules should not be empty
+    modules.collectFirst {
+      case m if m.getClass.getName == "com.fasterxml.jackson.module.scala.DefaultScalaModule" => m
+    } should not be empty
+  }
+}
