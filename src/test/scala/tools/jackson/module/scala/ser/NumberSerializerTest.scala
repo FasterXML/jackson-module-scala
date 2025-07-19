80eac814f92d9d83acd382e84a8ba8c793dd6d2b
PJ Fanning <pjfanning@users.noreply.github.com>
2025-07-19 09:32:03 +0100

    Create NumberSerializerTest.scala (#748)
    * Create NumberSerializerTest.scala
    
    * Update CaseClassSerializerTest.scala

---
+++ src/test/scala/tools/jackson/module/scala/ser/NumberSerializerTest.scala
@@ -0,0 +1,34 @@
+package tools.jackson.module.scala.ser
+
+import com.fasterxml.jackson.annotation.JsonFormat
+import tools.jackson.databind.cfg.MutableConfigOverride
+import tools.jackson.module.scala.{DefaultScalaModule, JacksonModule}
+
+class NumberSerializerTest extends SerializerTest {
+
+  val module: JacksonModule = DefaultScalaModule
+
+  "NumberSerializer" should "serialize BigDecimal as a number" in {
+    serialize(BigDecimal("123.456")) shouldBe "123.456"
+  }
+
+  it should "serialize BigDecimal as a number (JsonFormat.Shape.STRING)" in {
+    val builder = newBuilder
+      .withConfigOverride(classOf[BigDecimal],
+        (c: MutableConfigOverride) => c.setFormat(JsonFormat.Value.forShape(JsonFormat.Shape.STRING)))
+    serialize(BigDecimal("123.456"), builder.build()) shouldBe """"123.456""""
+  }
+
+  it should "serialize BigInt as a number" in {
+    serialize(BigInt("12345678901234567890")) shouldBe "12345678901234567890"
+  }
+
+  it should "serialize Int as a number" in {
+    serialize(42) shouldBe "42"
+  }
+
+  it should "serialize Long as a number" in {
+    serialize(12345678901234L) shouldBe "12345678901234"
+  }
+
+}
