c5d73e833d106f0d48d29067081bee30df9a90fb
PJ Fanning <pjfanning@users.noreply.github.com>
2025-10-23 21:50:15 +0100

    Sort order tests (#774)
    * fix test case where scala user wants alphabetic sorting of properties (#773)
    
    * add test

--- src/test/scala/com/fasterxml/jackson/module/scala/ser/CaseClassSerializerTest.scala
+++ src/test/scala/com/fasterxml/jackson/module/scala/ser/CaseClassSerializerTest.scala
@@ -2,7 +2,7 @@ package com.fasterxml.jackson.module.scala.ser
 
 import com.fasterxml.jackson.annotation.JsonProperty.Access
 import com.fasterxml.jackson.annotation._
-import com.fasterxml.jackson.databind.{ObjectMapper, PropertyNamingStrategies}
+import com.fasterxml.jackson.databind.{MapperFeature, ObjectMapper, PropertyNamingStrategies}
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 
 import scala.beans.BeanProperty
@@ -64,6 +64,9 @@ case class ClassWithOnlyUnitField(field: Unit)
 
 object CaseClassSerializerTest {
   case class BigDecimalHolder(bigDecimal: BigDecimal)
+  case class ClassWithUnorderedFields(f3: Int = 3, f2: Int = 2, f0: Int = 0, f1: Int = 1)
+  @JsonPropertyOrder(Array("f0", "f1", "f2", "f3"))
+  case class AnnotatedClassWithUnorderedFields(f3: Int = 3, f2: Int = 2, f0: Int = 0, f1: Int = 1)
 }
 
 class CaseClassSerializerTest extends SerializerTest {
@@ -224,4 +227,16 @@ class CaseClassSerializerTest extends SerializerTest {
     serialize(BigDecimalHolder(BigDecimal("123.456")), mapper) shouldEqual """{"bigDecimal":"123.456"}"""
   }
 
+  // https://github.com/FasterXML/jackson-module-scala/issues/772
+  it should "sort properties of the case class" ignore {
+    val mapper = newBuilder
+      .enable(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY)
+      .disable(MapperFeature.SORT_CREATOR_PROPERTIES_FIRST)
+      .build()
+    serialize(ClassWithUnorderedFields(), mapper) shouldEqual """{"f0":0,"f1":1,"f2":2,"f3":3}"""
+  }
+
+  it should "respect JsonPropertyOrder" in {
+    serialize(AnnotatedClassWithUnorderedFields(), newMapper) shouldEqual """{"f0":0,"f1":1,"f2":2,"f3":3}"""
+  }
 }
