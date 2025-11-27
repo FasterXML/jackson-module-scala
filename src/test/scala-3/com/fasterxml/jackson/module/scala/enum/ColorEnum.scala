f4b86f8cf308a4f760424336b1403f280ea2bbb2
PJ Fanning <pjfanning@users.noreply.github.com>
2025-11-27 20:08:02 +0100

    Update ColorEnum.scala

--- src/test/scala-3/com/fasterxml/jackson/module/scala/enum/ColorEnum.scala
+++ src/test/scala-3/com/fasterxml/jackson/module/scala/enum/ColorEnum.scala
@@ -4,6 +4,4 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 enum ColorEnum { case Red, Green, Blue }
 
-enum AnnotatedColorEnum { @JsonProperty("red") Red, @JsonProperty("green") Green, @JsonProperty("blue") Blue }
-
 case class Colors(set: Set[ColorEnum])
